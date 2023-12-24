1600fc46912bf65b2949f38f3c1600e68621570f
AminZamani <aminzamani.work@gmail.com>
2023-12-24 18:50:19 +0330

    feat(core), set a signal on create a user
    I will use it for send email activation OTP
    
    version: 0.2.4

---
+++ apps/core/signals.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def send_activation_email(sender, instance, created, **kwargs):
+    if created:
+        # TODO send activation email
+        print("email sent")
