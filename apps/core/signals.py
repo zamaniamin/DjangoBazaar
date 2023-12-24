8e0aa5b5929a7411fee83690a6b2df5db41eefe5
AminZamani <aminzamani.work@gmail.com>
2023-12-25 02:20:01 +0330

    feat(core), send email with django signal
    version: 0.2.6

--- apps/core/signals.py
+++ apps/core/signals.py
@@ -2,9 +2,10 @@ from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from apps.core.services.email.email_service import EmailService
+
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def send_activation_email(sender, instance, created, **kwargs):
     if created:
-        # TODO send activation email
-        print("email sent")
+        EmailService.send_activation_email(instance.email)
