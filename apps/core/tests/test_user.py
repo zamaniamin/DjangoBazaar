ea53120454c46e7df80ed2d39b54b408bd7b9fe4
AminZamani <aminzamani.work@gmail.com>
2023-12-24 17:00:54 +0330

    test(core), add unittest for create user
    version: 0.2.4

---
+++ apps/core/tests/test_user.py
@@ -0,0 +1,34 @@
+from django.contrib.auth import get_user_model
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+
+class UserCreateViewTest(APITestCase):
+    def setUp(self):
+        self.base_url = "/auth/users/"
+        self.user = get_user_model()
+
+    def test_create_user(self):
+        """
+        Test that we can create a new user.
+        """
+
+        # --- request ---
+        payload = {
+            "email": "user_test@example.com",
+            "password": "Test_1234",
+            "password_confirm": "Test_1234",
+        }
+        response = self.client.post(self.base_url, payload)
+
+        # --- expected ---
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertFalse("password" in response.data)
+        self.assertFalse("password_confirm" in response.data)
+        self.assertTrue("email" in response.data)
+        self.assertTrue("user_id" in response.data)
+        user = self.user.objects.get(email=payload["email"])
+        self.assertTrue(user.check_password(payload["password"]))
+        self.assertFalse(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
