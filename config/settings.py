27c7b4ab81ed94e95d826ddd25d074b1bdd637a3
AminZamani <aminzamani.work@gmail.com>
2023-12-26 19:07:19 +0330

    chore(core), remove a comment
    version: 0.2.12

--- config/settings.py
+++ config/settings.py
@@ -160,7 +160,6 @@ EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 # -------------------------
 
 REST_FRAMEWORK = {
-    # YOUR SETTINGS
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
