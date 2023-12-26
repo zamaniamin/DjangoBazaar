71e20abf8a6904368924c6bf37d6accb5bf37084
AminZamani <aminzamani.work@gmail.com>
2023-12-26 19:35:21 +0330

    feat(core), add JWT URIs
    version: 0.2.13

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,8 +1,16 @@
+from django.urls import re_path
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt import views
 
-from . import views
+from apps.core.views import UserViewSet
 
 router = DefaultRouter()
-router.register("users", views.UserViewSet)
+router.register("users", UserViewSet)
 
 urlpatterns = router.urls
+
+urlpatterns += [
+    re_path(r"^jwt/create/?", views.TokenObtainPairView.as_view(), name="jwt-create"),
+    re_path(r"^jwt/refresh/?", views.TokenRefreshView.as_view(), name="jwt-refresh"),
+    re_path(r"^jwt/verify/?", views.TokenVerifyView.as_view(), name="jwt-verify"),
+]
