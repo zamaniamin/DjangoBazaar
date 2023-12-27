a81850b03b3441850b894b0e4b72d182204f08bb
AminZamani <aminzamani.work@gmail.com>
2023-12-27 19:13:43 +0330

    feat(core), update faker user populations
    version: 0.2.18

--- apps/core/faker/user_faker.py
+++ apps/core/faker/user_faker.py
@@ -1,12 +1,11 @@
 from faker import Faker
 
-from apps.core.managers.user_manager import UserManager
 from apps.core.models import User
 
 
 class FakeUser:
     """
-    Populates the database with fake accounts.
+    Populates the database with fake users.
     """
 
     fake = Faker()
@@ -17,77 +16,38 @@ class FakeUser:
         return cls.fake.email()
 
     @classmethod
-    def create_inactive_user(cls):
+    def populate_superuser(cls):
         """
-        Register a new user and get the OTP code.
+        Create a new superuser.
         """
 
-        # --- register a user ---
-        register_payload = {
-            'email': cls.random_email(),
-            'password': cls.password
-        }
-        return UserManager.create_inactive_user(**register_payload)
+        user = User.objects.create_user(email=cls.random_email(), password=cls.password, is_superuser=True,
+                                        is_staff=True)
+        return user
 
     @classmethod
-    def create_active_user(cls):
+    def populate_admin(cls):
         """
-        Registered a new user and verified their OTP code.
+        Creat a new admin.
         """
+
+        user = User.objects.create_user(email=cls.random_email(), password=cls.password, is_staff=True)
+        return user
+
+    @classmethod
+    def populate_user(cls):
+        """
+        Create a new user.
+        """
+
         user = User.objects.create_user(email=cls.random_email(), password=cls.password)
         return user
 
-    # @classmethod
-    # def populate_members(cls):
-    #     """
-    #     Create an admin and a user.
-    #     """
-    #
-    #     # --- admin ---
-    #     user, access_token = FakeAccount.verified_registration()
-    #     user_data = {
-    #         'email': 'superuser@example.com',
-    #         'first_name': cls.fake.first_name(),
-    #         'last_name': cls.fake.last_name(),
-    #         'is_superuser': True,
-    #         'role': 'admin'
-    #     }
-    #
-    #     UserService.update_user(user.id, **user_data)
-    #
-    #     # --- user ---
-    #     user, access_token = FakeAccount.verified_registration()
-    #     user_data = {
-    #         'email': 'member@example.com',
-    #         'first_name': cls.fake.first_name(),
-    #         'last_name': cls.fake.last_name()
-    #     }
-    #
-    #     UserService.update_user(user.id, **user_data)
+    @classmethod
+    def populate_inactive_user(cls):
+        """
+        Create a new inactive user.
+        """
 
-    # @classmethod
-    # def populate_superuser(cls):
-    #     """
-    #     Create an admin and generate an access token too.
-    #     """
-    #
-    #     user, access_token = FakeAccount.verified_registration()
-    #     user = UserService.update_user(user.id)
-    #     UserService.set_role(user.id, 'superuser')
-    #
-    #     return user, access_token
-
-    # @classmethod
-    # def populate_user(cls):
-    #     """
-    #     Create a new user and generate an access token too.
-    #     """
-    #
-    #     user, access_token = FakeAccount.verified_registration()
-    #     user_data = {
-    #         'first_name': cls.fake.first_name(),
-    #         'last_name': cls.fake.last_name()
-    #     }
-    #
-    #     user = UserService.update_user(user.id, **user_data)
-    #     return user, access_token
+        user = User.objects.create_user(email=cls.random_email(), password=cls.password, is_active=False)
+        return user
