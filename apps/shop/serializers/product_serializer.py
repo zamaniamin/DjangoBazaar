deedcd7f8560ac6cd6fac2457830bd0cc0a5537b
AminZamani <aminzamani.work@gmail.com>
2023-12-30 01:32:53 +0330

    chore(config), rename product_serializer.py to product_serializers.py
    version: 0.3.5

--- apps/shop/serializers/product_serializer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from apps.shop.models import Product
-
-
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = '__all__'
