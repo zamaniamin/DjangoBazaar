17a5804a9e65af0f300065e1a1e414558823c7d0
AminZamani <aminzamani.work@gmail.com>
2023-12-29 16:40:27 +0330

    feat(core), add demo user to faker
    version: 0.2.29

--- apps/core/faker/user_faker.py
+++ apps/core/faker/user_faker.py
@@ -9,11 +9,26 @@ class FakeUser:
     """
 
     fake = Faker()
-    password = 'Demo_1234'
+
+    # --- demo users ---
+    superuser_email = 'super@test.test'
+    admin_email = 'admin@test.test'
+    user1_email = 'user1@test.test'
+    user2_email = 'user2@test.test'
+    user3_email = 'user3@test.test'
+    password = 'user1234'
 
     @classmethod
     def random_email(cls):
         return cls.fake.email()
+
+    @classmethod
+    def populate_demo_users(cls):
+        cls.populate_superuser()
+        cls.populate_admin()
+        cls.populate_user(cls.user1_email)
+        cls.populate_user(cls.user2_email)
+        cls.populate_inactive_user(cls.user3_email)
 
     @classmethod
     def populate_superuser(cls):
@@ -21,7 +36,7 @@ class FakeUser:
         Create a new superuser.
         """
 
-        user = User.objects.create_user(email=cls.random_email(), password=cls.password, is_superuser=True,
+        user = User.objects.create_user(email=cls.superuser_email, password=cls.password, is_superuser=True,
                                         is_staff=True)
         return user
 
@@ -31,23 +46,28 @@ class FakeUser:
         Creat a new admin.
         """
 
-        user = User.objects.create_user(email=cls.random_email(), password=cls.password, is_staff=True)
+        user = User.objects.create_user(email=cls.admin_email, password=cls.password, is_staff=True)
         return user
 
     @classmethod
-    def populate_user(cls):
+    def populate_user(cls, email=None):
         """
         Create a new user.
         """
+        if not email:
+            email = cls.random_email()
 
-        user = User.objects.create_user(email=cls.random_email(), password=cls.password)
+        user = User.objects.create_user(email=email, password=cls.password)
         return user
 
     @classmethod
-    def populate_inactive_user(cls):
+    def populate_inactive_user(cls, email=None):
         """
         Create a new inactive user.
         """
 
-        user = User.objects.create_user(email=cls.random_email(), password=cls.password, is_active=False)
+        if not email:
+            email = cls.random_email()
+
+        user = User.objects.create_user(email=email, password=cls.password, is_active=False)
         return user
