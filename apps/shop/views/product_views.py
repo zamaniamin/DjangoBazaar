e2a5ca78f855384eabf39d363bd926587aa9d63e
AminZamani <aminzamani.work@gmail.com>
2024-01-04 19:25:20 +0330

    refactor(shop/product), remove extra codes
    version: 0.3.19

--- apps/shop/views/product_views.py
+++ apps/shop/views/product_views.py
@@ -3,7 +3,6 @@ from rest_framework import viewsets, status
 from rest_framework.permissions import IsAdminUser, AllowAny
 from rest_framework.response import Response
 
-from apps.core.services.time_service import DateTime
 from apps.shop.models import Product, ProductVariant
 from apps.shop.serializers import product_serializers as s
 from apps.shop.services.product_service import ProductService
@@ -46,51 +45,8 @@ class ProductView(viewsets.ModelViewSet):
         # --- create product ---
         product = ProductService.create_product(**payload)
 
-        # --- response ---
-        response_body = {
-            "product_id": product.id,
-            "product_name": product.product_name,
-            "description": product.description,
-            "status": product.status,
-            "options": self.__dict_product_options(product.productoption_set.all()),
-            "variants": self.__dict_product_variants(product.productvariant_set.all()),
-            "created_at": DateTime.string(product.created_at),
-            "updated_at": DateTime.string(product.updated_at),
-            "published_at": DateTime.string(product.published_at),
-        }
-        return Response(response_body, status=status.HTTP_201_CREATED)
+        # Serialize the created product for the response
+        response_serializer = s.ProductSerializer(product)
 
-    @staticmethod
-    def __dict_product_options(options):
-        product_options = []
-        for option in options:
-            items = option.productoptionitem_set.all()
-
-            product_options.append({
-                'option_id': option.id,
-                'option_name': option.option_name,
-                'items': [{'item_id': item.id, 'item_name': item.item_name} for item in items]
-            })
-        if product_options:
-            return product_options
-        else:
-            return None
-
-    @staticmethod
-    def __dict_product_variants(variants):
-        product_variants = []
-        for variant in variants:
-            product_variants.append({
-                "variant_id": variant.id,
-                "price": variant.price,
-                "stock": variant.stock,
-                "option1": variant.option1.item_name if variant.option1 else None,
-                "option2": variant.option2.item_name if variant.option2 else None,
-                "option3": variant.option3.item_name if variant.option3 else None,
-                "created_at": DateTime.string(variant.created_at),
-                "updated_at": DateTime.string(variant.updated_at)
-            })
-
-        if product_variants:
-            return product_variants
-        return None
+        # Return the serialized response
+        return Response(response_serializer.data, status=status.HTTP_201_CREATED)
