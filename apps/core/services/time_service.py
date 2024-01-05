c6e99ca6e06aa72b6372830f5d4787cd61582752
AminZamani <aminzamani.work@gmail.com>
2024-01-05 22:07:35 +0330

    docs(shop/core), add doc to DateTime service
    remove extra methods
    
    version: 0.3.19

--- apps/core/services/time_service.py
+++ apps/core/services/time_service.py
@@ -8,21 +8,12 @@ class DateTime:
         """
         Convert a datetime object to a formatted string.
 
-        This method takes a datetime object `obj` and converts it into a string
-        in the format 'YYYY-MM-DD HH:MM:SS'. If `obj` is None or evaluates to False,
-        it returns None.
+        Args:
+            obj (datetime or None): The datetime object to be converted to a string.
 
-        Parameters:
-        cls (object): An instance of the class (although this argument is not used).
-        obj (datetime or None): The datetime object to be converted to a string.
+        Returns: str or None: A formatted string representation of the datetime object, or None if the input is None
+        or evaluates to False.
 
-        Returns:
-        str or None: A formatted string representation of the datetime object,
-                     or None if the input is None or evaluates to False.
         """
 
         return obj.strftime('%Y-%m-%d %H:%M:%S') if obj else None
-
-    @classmethod
-    def now(cls):
-        return datetime.strptime(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), '%Y-%m-%d %H:%M:%S')
