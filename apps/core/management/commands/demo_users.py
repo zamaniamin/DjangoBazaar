be5ee23bdcbfd1fe19278706a279d5a8e01b555a
AminZamani <aminzamani.work@gmail.com>
2024-01-05 19:42:49 +0330

    docs(shop/core), add docs to `Command`
    version: 0.3.19

--- apps/core/management/commands/demo_users.py
+++ apps/core/management/commands/demo_users.py
@@ -5,9 +5,20 @@ from apps.core.faker.user_faker import FakeUser
 
 class Command(BaseCommand):
     """
-    usage: python manage.py demo_users
+    Custom management command to populate the database with initial data.
+
+    Usage:
+        python manage.py demo_users
     """
     help = 'Populates the database with initial data'
 
     def handle(self, *args, **options):
+        """
+        Handle method to execute the population of demo users.
+
+        Args:
+            *args: Additional command-line arguments.
+            **options: Additional options.
+
+        """
         FakeUser.populate_demo_users()
