d04e4d22f7e1cba4df0721e9f3955cbfa36a8d4c
AminZamani <aminzamani.work@gmail.com>
2024-01-11 02:51:32 +0330

    test(shop/product), add new retrieve scenario
    version: 0.3.23

--- apps/shop/tests/test_product/test_retrieve_product.py
+++ apps/shop/tests/test_product/test_retrieve_product.py
@@ -60,3 +60,86 @@ class RetrieveProductTest(ProductBaseTestCase):
             expected_price=self.simple_product_payload["price"],
             expected_stock=self.simple_product_payload["stock"],
         )
+
+    def test_retrieve_product_with_options(self):
+        """
+        Test retrieve a product with options:
+        - with price and stock.
+        - with options
+        - with variants.
+        - no media.
+        """
+
+        # --- request ---
+        response = self.client.get(f"{self.product_path}{self.variable_product.id}/")
+
+        # --- expected product ---
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        expected = response.json()
+        self.assertIsInstance(expected["id"], int)
+        self.assertEqual(
+            expected["product_name"], self.variable_product_payload["product_name"]
+        )
+        self.assertEqual(
+            expected["description"], self.variable_product_payload["description"]
+        )
+        self.assertEqual(expected["status"], self.variable_product_payload["status"])
+
+        # --- expected product date and time ---
+        self.assertExpectedDatetimeFormat(expected)
+
+        # --- expected product options ---
+        self.assertEqual(len(expected["options"]), 3)
+        self.assertExpectedOptions(
+            expected["options"], self.variable_product_payload["options"]
+        )
+
+        # --- expected product variants ---
+        self.assertTrue(len(expected["variants"]) == 8)
+        self.assertExpectedVariants(
+            expected["variants"],
+            self.variable_product_payload["price"],
+            self.variable_product_payload["stock"],
+        )
+
+    def test_404(self):
+        """
+        Test retrieve a product if it doesn't exist.
+        """
+        response = self.client.get(f"{self.product_path}{999999999}/")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_retrieve_product_by_admin(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f"JWT {self.admin_access_token}")
+        response = self.client.get(f"{self.product_path}{self.simple_product.id}/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_list_product_by_admin(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f"JWT {self.admin_access_token}")
+        response = self.client.get(self.product_path)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_retrieve_product_by_user(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f"JWT {self.member_access_token}")
+        response = self.client.get(f"{self.product_path}{self.simple_product.id}/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_list_product_by_user(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f"JWT {self.member_access_token}")
+        response = self.client.get(self.product_path)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_retrieve_product_by_guest(self):
+        self.client.credentials()
+        response = self.client.get(f"{self.product_path}{self.simple_product.id}/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_list_product_by_guest(self):
+        self.client.credentials()
+        response = self.client.get(self.product_path)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+# TODO test_with_media
+# TODO test_with_options_media
+
+
