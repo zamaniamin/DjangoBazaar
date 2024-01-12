1c08c9f041ddc222cba662ede917d7c887a043d3
AminZamani <aminzamani.work@gmail.com>
2024-01-12 22:57:11 +0330

    test(core/user), refactor create user
    version: 0.3.30

---
+++ apps/core/tests/test_create_user.py
@@ -0,0 +1,37 @@
+from django.contrib.auth import get_user_model
+from django.core import mail
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+
+class CreateUserTest(APITestCase):
+    def test_create_user_or_register(self):
+        """
+        Test that we can create a new user. (register a new user with email and password)
+        """
+
+        # --- request ---
+        payload = {
+            "email": "user_test@example.com",
+            "password": "Test_1234",
+            "password_confirm": "Test_1234",
+        }
+        response = self.client.post(reverse("user-list"), payload)
+
+        # --- expected ---
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertFalse("password" in response.data)
+        self.assertFalse("password_confirm" in response.data)
+        self.assertTrue("email" in response.data)
+        self.assertTrue("id" in response.data)
+        user = get_user_model().objects.get(email=payload["email"])
+        self.assertTrue(user.check_password(payload["password"]))
+        self.assertFalse(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+        # --- expected email ---
+        expected_mail = mail.outbox
+        self.assertEqual(len(expected_mail), 1)
+        self.assertEqual(expected_mail[0].to, [payload["email"]])
--- apps/core/tests/test_user.py
+++ apps/core/tests/test_user.py
@@ -23,40 +23,7 @@ class UserViewTest(APITestCase):
 
         self.inactive_user = FakeUser.populate_inactive_user()
 
-    # -------------------
-    # --- test create ---
-    # -------------------
 
-    def test_create_user_or_register(self):
-        """
-        Test that we can create a new user.
-        (register a new user with email and password)
-        """
-
-        # --- request ---
-        payload = {
-            "email": "user_test@example.com",
-            "password": "Test_1234",
-            "password_confirm": "Test_1234",
-        }
-        response = self.client.post(self.base_url, payload)
-
-        # --- expected ---
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertFalse("password" in response.data)
-        self.assertFalse("password_confirm" in response.data)
-        self.assertTrue("email" in response.data)
-        self.assertTrue("id" in response.data)
-        user = get_user_model().objects.get(email=payload["email"])
-        self.assertTrue(user.check_password(payload["password"]))
-        self.assertFalse(user.is_active)
-        self.assertFalse(user.is_staff)
-        self.assertFalse(user.is_superuser)
-
-        # --- expected email ---
-        expected_mail = mail.outbox
-        self.assertEqual(len(expected_mail), 4)
-        self.assertEqual(expected_mail[3].to, [payload["email"]])
 
     # -----------------
     # --- test list ---
