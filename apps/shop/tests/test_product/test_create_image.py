f8a4d65ea3e8992f25359b41381ea329c06152ba
AminZamani <aminzamani.work@gmail.com>
2024-01-18 01:24:42 +0330

    test(shop/product), update test, image upload
    version: 0.3.41

--- apps/shop/tests/test_product/test_create_image.py
+++ apps/shop/tests/test_product/test_create_image.py
@@ -10,32 +10,35 @@ from apps.shop.tests.test_product.base_test_case import ProductBaseTestCase
 from config import settings
 
 
-class RetrieveProductTest(ProductBaseTestCase):
+class CreateImageTest(ProductBaseTestCase):
+    files: list
+
     @classmethod
     def setUpTestData(cls):
         super().setUpTestData()
         cls.active_product = ProductFaker.populate_active_product()
+        cls.files = cls.generate_list_photo_files()
+        cls.file_count = len(cls.files)
 
     def setUp(self):
         self.client.credentials(HTTP_AUTHORIZATION=f"JWT {self.admin_access_token}")
 
     def test_images_upload_success(self):
-        # --- init files ---
-        files = self.generate_list_photo_files()
-        file_count = len(files)
-
         # --- request ---
-        payload = {"images": files}
+        payload = {"images": self.files}
         response = self.client.post(
-            reverse("product-images-upload", kwargs={"pk": self.active_product.id}),
+            reverse(
+                "product-images-list", kwargs={"product_pk": self.active_product.id}
+            ),
             payload,
+            format="multipart",
         )
 
         # --- expected ---
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         expected = response.json()
         self.assertIsInstance(expected, list)
-        self.assertEqual(len(expected), file_count)
+        self.assertEqual(len(expected), self.file_count)
 
         for image in expected:
             self.assertIsInstance(image["id"], int)
@@ -51,7 +54,7 @@ class RetrieveProductTest(ProductBaseTestCase):
 
         # Check if the images have been added to the product
         product_media = ProductMedia.objects.filter(product=self.active_product)
-        self.assertEqual(product_media.count(), file_count)
+        self.assertEqual(product_media.count(), self.file_count)
 
         # Check if the response data matches the serialized ProductMedia
         expected_data = ProductImageSerializer(product_media, many=True).data
