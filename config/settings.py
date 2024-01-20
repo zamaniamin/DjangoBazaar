b967be273c76275c22ca2c32b7f7f36d4239473b
AminZamani <aminzamani.work@gmail.com>
2024-01-20 20:03:57 +0330

    feat(config), add django-filter to installed apps
    version: 0.3.45

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django.contrib.sites",
     # --- External Packages ---
+    "django_filters",
     "rest_framework",
     "drf_spectacular",
     "debug_toolbar",
