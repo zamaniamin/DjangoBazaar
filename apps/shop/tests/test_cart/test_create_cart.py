f996baeb9f72357385a2af1b9bd4df7dd5433900
AminZamani <aminzamani.work@gmail.com>
2024-01-23 07:16:55 +0330

    test(shop/cart), update test case
    version: 0.3.57

--- apps/shop/tests/test_cart/test_create_cart.py
+++ apps/shop/tests/test_cart/test_create_cart.py
@@ -1,3 +1,4 @@
+import json
 import uuid
 
 from django.urls import reverse
@@ -5,19 +6,10 @@ from rest_framework import status
 
 from apps.core.tests.base_test import BaseCoreTestCase
 from apps.shop.faker.product_faker import SimpleProductFaker, VariableProductFaker
+from apps.shop.models.cart import Cart, CartItem
 
 
 class CreateCartTest(BaseCoreTestCase):
-    @classmethod
-    def setUpTestData(cls):
-        # create some products
-        cls.simple_product = (
-            SimpleProductFaker.populate_active_simple_product_with_image()
-        )
-        cls.variable_product = (
-            VariableProductFaker.populate_active_variable_product_with_image()
-        )
-
     def test_create_cart(self):
         response = self.client.post(
             reverse("cart-list"), {}, content_type="application/json"
@@ -32,13 +24,112 @@ class CreateCartTest(BaseCoreTestCase):
 
 
 class CreateCartItemsTest(BaseCoreTestCase):
-    ...
+    simple_product = None
+    variable_product = None
+
+    @classmethod
+    def setUpTestData(cls):
+        # --- simple product ---
+        cls.simple_product = (
+            SimpleProductFaker.populate_active_simple_product_with_image()
+        )
+        cls.simple_product_variant = cls.simple_product.variants.first()
+
+        # --- variable product ---
+        cls.variable_product = (
+            VariableProductFaker.populate_active_variable_product_with_image(
+                random_options=False
+            )
+        )
+        cls.variable_product_variants = list(cls.variable_product.variants.all())
+
+        # --- cart ---
+        cart = Cart.objects.create()
+        cls.cart_id = cart.id
+
+    def test_create_one_cart_item(self):
+        # --- request ---
+        payload = {"variant": self.simple_product_variant.id, "quantity": 1}
+        response = self.client.post(
+            reverse("cart-items-list", kwargs={"cart_pk": self.cart_id}),
+            json.dumps(payload),
+            content_type="application/json",
+        )
+
+        # --- expected ---
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        # TODO test items data
+        # TODO test item_total value
+        # TODO test total_price value
+        # print(response.data)
+
+    def test_create_cart_items(self):
+        # --- request ---
+        for variant in self.variable_product_variants:
+            payload = {"variant": variant.id, "quantity": 1}
+            response = self.client.post(
+                reverse("cart-items-list", kwargs={"cart_pk": self.cart_id}),
+                json.dumps(payload),
+                content_type="application/json",
+            )
+            self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # --- expected ---
+        # response = self.client.get(reverse("cart-list"))
+        # self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # self.assertEqual(len())
+        # print(response.json())
 
 
-# TODO test create new cart item
+class RetrieveCartTest(BaseCoreTestCase):
+    simple_product = None
+    variable_product = None
+    variable_product_variants = None
+
+    @classmethod
+    def setUpTestData(cls):
+        # --- simple product ---
+        cls.simple_product = (
+            SimpleProductFaker.populate_active_simple_product_with_image()
+        )
+        cls.simple_product_variant = cls.simple_product.variants.first()
+
+        # --- variable product ---
+        cls.variable_product = (
+            VariableProductFaker.populate_active_variable_product_with_image(
+                random_options=False
+            )
+        )
+        cls.variable_product_variants = list(cls.variable_product.variants.all())
+
+        # --- cart ---
+        cart = Cart.objects.create()
+        cls.cart_id = cart.id
+        for variant in cls.variable_product_variants:
+            CartItem.objects.create(
+                cart_id=cart.id,
+                variant=variant.variant,
+                quantity=1
+            )
+
+    def test_retrieve_cart(self):
+        # TODO only admins can see cart (read only)
+        response = self.client.get(reverse("cart-list"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    # def test_retrieve
+
+
+# TODO test retrieve cart
+# TODO test retrieve cart item
 # TODO test remove cart
 # TODO test remove cart item
 # TODO test update cart item quantity
 # TODO test cart total price
 # TODO test item total price
+# TODO test access permissions
+# TODO fix error 500 on create cart items ['“7” is not a valid UUID.']
+# TODO fix error 500 on create cart items if uuid dos not exist [FOREIGN KEY constraint failed], should return 404
+# TODO rename guest users in test to Anonymous user
+# TODO add cart to faker
 # print(response.data)
