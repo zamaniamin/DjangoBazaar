c9196460b84c04fd90717ef7115b46be1e42e7ca
AminZamani <aminzamani.work@gmail.com>
2024-01-26 02:46:35 +0330

    test(core/user), refactor list and retrieve test
    version: 0.3.58

--- apps/core/tests/test_user/test_retrieve_user.py
+++ apps/core/tests/test_user/test_retrieve_user.py
@@ -5,46 +5,6 @@ from apps.core.tests.base_test import CoreBaseTestCase
 
 
 class RetrieveUserBaseTest(CoreBaseTestCase):
-    # TODO separate list to new test file
-    # ------------------
-    # --- list users ---
-    # ------------------
-
-    def test_list_users_by_admin(self):
-        # request
-        self.set_admin_user_authorization()
-        response = self.client.get(reverse("user-list"))
-
-        # expected
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        expected = response.json()
-        self.assertEqual(len(expected), 2)
-        for user in expected:
-            self.assertEqual(len(user), 7)
-            self.assertIsInstance(user["id"], int)
-            self.assertIsInstance(user["email"], str)
-            self.assertIsInstance(user["first_name"], str)
-            self.assertIsInstance(user["last_name"], str)
-            self.assertIsInstance(user["is_active"], bool)
-            self.assertDatetimeFormat(user["date_joined"])
-            self.assertTrue(
-                user["last_login"] is None
-                or self.assertDatetimeFormat(user["last_login"])
-            )
-
-    def test_list_users_by_regular_user(self):
-        self.set_regular_user_authorization()
-        response = self.client.get(reverse("user-list"))
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-    def test_list_users_by_anonymous_user(self):
-        response = self.client.get(reverse("user-list"))
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-
-    # ---------------------
-    # --- retrieve user ---
-    # ---------------------
-
     def test_retrieve_user_by_admin(self):
         self.set_admin_user_authorization()
 
