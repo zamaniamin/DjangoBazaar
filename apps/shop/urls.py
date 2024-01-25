286aafbbf974b8cbd1c60628f267d079a078ad90
AminZamani <aminzamani.work@gmail.com>
2024-01-25 05:52:56 +0330

    refactor(shop/cart), refactor to new directory
    version: 0.3.58

--- apps/shop/urls.py
+++ apps/shop/urls.py
@@ -1,6 +1,6 @@
 from rest_framework_nested import routers
 
-from apps.shop.views.product_views.cart_view import CartViewSet, CartItemViewSet
+from apps.shop.views.cart_views.cart_view import CartViewSet, CartItemViewSet
 from apps.shop.views.product_views.image_view import ProductImageViewSet
 from apps.shop.views.product_views.product_view import ProductViewSet
 from apps.shop.views.product_views.variant_view import VariantViewSet
