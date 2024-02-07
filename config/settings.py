e495995eab7d7d516470182fc47e8ba8cbae449c
AminZamani <aminzamani.work@gmail.com>
2024-02-07 11:08:41 +0330

    feat(config) add new env in settings
    version: 0.3.59

--- config/settings.py
+++ config/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS")
 
 # Application definition
 
