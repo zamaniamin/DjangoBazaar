c2f38543ba976eebb87bf8de727469de8420f637
AminZamani <aminzamani.work@gmail.com>
2024-02-10 09:50:39 +0330

    test(shop/cart): refactor test to separate test files
    version: 0.3.60

--- apps/shop/tests/test_cart/test_create_cart_items.py
+++ apps/shop/tests/test_cart/test_create_cart_items.py
@@ -1,5 +1,4 @@
 import json
-import uuid
 
 from django.urls import reverse
 from rest_framework import status
@@ -7,20 +6,6 @@ from rest_framework import status
 from apps.core.tests.base_test import CoreBaseTestCase
 from apps.shop.demo.factory.product.product_factory import ProductFactory
 from apps.shop.models.cart import Cart
-
-
-class CreateCartBaseTest(CoreBaseTestCase):
-    def test_create_cart(self):
-        response = self.client.post(
-            reverse("cart-list"), {}, content_type="application/json"
-        )
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        expected = response.json()
-        self.assertEqual(len(expected), 3)
-        self.assertIsInstance(uuid.UUID(expected["id"]), uuid.UUID)
-        self.assertIsInstance(expected["items"], list)
-        self.assertEqual(len(expected["items"]), 0)
-        self.assertEqual(expected["total_price"], 0)
 
 
 class CreateCartItemsBaseTest(CoreBaseTestCase):
