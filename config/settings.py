c9d9603bde7d20f46c9da58e6cb7e613c35ff1a2
AminZamani <aminzamani.work@gmail.com>
2024-02-13 12:00:44 +0330

    fix(config): update static-file settings
    version: 0.3.64

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Take environment variables from .env file
 env = environ.Env()
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -29,7 +29,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env.str("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG')
+DEBUG = env.bool("DEBUG")
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
@@ -132,6 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(env.str("STATIC_ROOT"), "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -241,19 +242,12 @@ CACHES = {
     }
 }
 
-# --------------
-# --- Static ---
-# --------------
-
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
-
 # -------------
 # --- Media ---
 # -------------
 
 MEDIA_URL = "media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = os.path.join(env.str("STATIC_ROOT"), "media")
 
 # ------------
 # --- ASGI ---
