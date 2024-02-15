434085900ba2fb64242323e5998967388cf09080
AminZamani <aminzamani.work@gmail.com>
2024-02-15 09:04:54 +0330

    refactor(core/jwt): remove "jwt" from uri
    version: 0.3.64

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -2,7 +2,7 @@ from django.urls import re_path
 from rest_framework_simplejwt import views
 
 urlpatterns = [
-    re_path(r"^jwt/create/?", views.TokenObtainPairView.as_view(), name="jwt-create"),
-    re_path(r"^jwt/refresh/?", views.TokenRefreshView.as_view(), name="jwt-refresh"),
-    re_path(r"^jwt/verify/?", views.TokenVerifyView.as_view(), name="jwt-verify"),
+    re_path(r"^create/?", views.TokenObtainPairView.as_view(), name="jwt-create"),
+    re_path(r"^refresh/?", views.TokenRefreshView.as_view(), name="jwt-refresh"),
+    re_path(r"^verify/?", views.TokenVerifyView.as_view(), name="jwt-verify"),
 ]
