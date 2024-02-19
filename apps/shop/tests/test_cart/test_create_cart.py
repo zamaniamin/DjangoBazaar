536288730cc5ea0f8d8af1b6dfcd806c1584f5c7
AminZamani <aminzamani.work@gmail.com>
2024-02-19 11:10:51 +0330

    test(shop/cart): change methods order
    version: 0.3.68

--- apps/shop/tests/test_cart/test_create_cart.py
+++ apps/shop/tests/test_cart/test_create_cart.py
@@ -7,18 +7,6 @@ from apps.core.tests.base_test import CoreBaseTestCase
 
 
 class CreateCartTest(CoreBaseTestCase):
-    def test_create_cart(self):
-        response = self.client.post(
-            reverse("cart-list"), {}, content_type="application/json"
-        )
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        expected = response.json()
-        self.assertEqual(len(expected), 3)
-        self.assertIsInstance(uuid.UUID(expected["id"]), uuid.UUID)
-        self.assertIsInstance(expected["items"], list)
-        self.assertEqual(len(expected["items"]), 0)
-        self.assertEqual(expected["total_price"], 0)
-
     def test_create_carts_by_admin(self):
         self.set_admin_user_authorization()
         response = self.client.post(reverse("cart-list"))
@@ -33,3 +21,15 @@ class CreateCartTest(CoreBaseTestCase):
         self.set_anonymous_user_authorization()
         response = self.client.post(reverse("cart-list"))
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_cart(self):
+        response = self.client.post(
+            reverse("cart-list"), {}, content_type="application/json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        expected = response.json()
+        self.assertEqual(len(expected), 3)
+        self.assertIsInstance(uuid.UUID(expected["id"]), uuid.UUID)
+        self.assertIsInstance(expected["items"], list)
+        self.assertEqual(len(expected["items"]), 0)
+        self.assertEqual(expected["total_price"], 0)
