b3b6a73dbf5d2f2b1a9e85a95efbe88428af5ca8
AminZamani <aminzamani.work@gmail.com>
2024-02-21 22:06:28 +0330

    test(shop/cart): add test case destroy cart and cart items
    version: 0.3.73

--- apps/shop/tests/test_cart/test_destroy_cart.py
+++ apps/shop/tests/test_cart/test_destroy_cart.py
@@ -1,3 +1,168 @@
-# TODO test remove cart
+from django.urls import reverse
+from rest_framework import status
+
+from apps.core.tests.base_test import CoreBaseTestCase
+from apps.shop.demo.factory.cart.cart_factory import CartFactory
+
+
+class DestroyCartTest(CoreBaseTestCase):
+    def setUp(self):
+        self.cart_id, self.cart_item = CartFactory.add_one_item(get_item=True)
+
+    # -------------------------------
+    # --- Test Access Permissions ---
+    # -------------------------------
+    def test_delete_cart_by_admin(self):
+        self.set_admin_user_authorization()
+        response = self.client.delete(
+            reverse("cart-detail", kwargs={"pk": self.cart_id})
+        )
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_delete_cart_by_regular_user(self):
+        self.set_regular_user_authorization()
+        response = self.client.delete(
+            reverse("cart-detail", kwargs={"pk": self.cart_id})
+        )
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_delete_cart_by_anonymous_user(self):
+        self.set_anonymous_user_authorization()
+        response = self.client.delete(
+            reverse("cart-detail", kwargs={"pk": self.cart_id})
+        )
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_delete_cart(self):
+        response = self.client.delete(
+            reverse("cart-detail", kwargs={"pk": self.cart_id})
+        )
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        # --- test cart is removed ---
+        response = self.client.get(reverse("cart-detail", kwargs={"pk": self.cart_id}))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+        # --- test cart items are removed ---
+        response = self.client.get(
+            reverse(
+                "cart-items-detail",
+                kwargs={"cart_pk": self.cart_id, "pk": self.cart_item.id},
+            )
+        )
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_delete_cart_with_invalid_pk(self):
+        response = self.client.delete(reverse("cart-detail", kwargs={"pk": 7}))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_delete_cart_with_multi_items(self):
+        self.cart_id, self.cart_items = CartFactory.add_multiple_items(get_items=True)
+        response = self.client.delete(
+            reverse("cart-detail", kwargs={"pk": self.cart_id})
+        )
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        # --- test cart is removed ---
+        response = self.client.get(reverse("cart-detail", kwargs={"pk": self.cart_id}))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+        # --- test cart items are removed ---
+        for item in self.cart_items:
+            response = self.client.get(
+                reverse(
+                    "cart-items-detail",
+                    kwargs={"cart_pk": self.cart_id, "pk": item.id},
+                )
+            )
+            self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+
+class DestroyCartItemsTest(CoreBaseTestCase):
+    def setUp(self):
+        self.cart_id, self.cart_item = CartFactory.add_one_item(get_item=True)
+
+    # -------------------------------
+    # --- Test Access Permissions ---
+    # -------------------------------
+    def test_delete_cart_item_by_admin(self):
+        self.set_admin_user_authorization()
+        response = self.client.delete(
+            reverse(
+                "cart-items-detail",
+                kwargs={"cart_pk": self.cart_id, "pk": self.cart_item.id},
+            )
+        )
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_delete_cart_item_by_regular_user(self):
+        self.set_regular_user_authorization()
+        response = self.client.delete(
+            reverse(
+                "cart-items-detail",
+                kwargs={"cart_pk": self.cart_id, "pk": self.cart_item.id},
+            )
+        )
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_delete_cart_item_by_anonymous_user(self):
+        self.set_anonymous_user_authorization()
+        response = self.client.delete(
+            reverse(
+                "cart-items-detail",
+                kwargs={"cart_pk": self.cart_id, "pk": self.cart_item.id},
+            )
+        )
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_delete_cart_item(self):
+        response = self.client.delete(
+            reverse(
+                "cart-items-detail",
+                kwargs={"cart_pk": self.cart_id, "pk": self.cart_item.id},
+            )
+        )
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        # --- test cart is removed ---
+        # response = self.client.get(reverse("cart-items-detail", kwargs={"pk": self.cart_id}))
+        # self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+        # --- test cart items are removed ---
+        response = self.client.get(
+            reverse(
+                "cart-items-detail",
+                kwargs={"cart_pk": self.cart_id, "pk": self.cart_item.id},
+            )
+        )
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_delete_cart_item_with_invalid_cart_pk(self):
+        response = self.client.delete(reverse("cart-detail", kwargs={"pk": 7}))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_delete_cart_with_multi_items(self):
+        self.cart_id, self.cart_items = CartFactory.add_multiple_items(get_items=True)
+        response = self.client.delete(
+            reverse("cart-detail", kwargs={"pk": self.cart_id})
+        )
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        # --- test cart is removed ---
+        response = self.client.get(reverse("cart-detail", kwargs={"pk": self.cart_id}))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+        # --- test cart items are removed ---
+        for item in self.cart_items:
+            response = self.client.get(
+                reverse(
+                    "cart-items-detail",
+                    kwargs={"cart_pk": self.cart_id, "pk": item.id},
+                )
+            )
+            self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+
 # TODO test remove cart item
-# TODO test access permissions
+# TODO test remove cart item with invalid cart id
+# TODO test access permissions on destroy cart items
