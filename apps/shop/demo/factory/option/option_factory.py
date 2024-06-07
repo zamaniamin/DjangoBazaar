b3fb1e826cb95fc1e888c8385dc61419fc65003b
AminZamani <aminzamani.work@gmail.com>
2024-06-07 16:09:14 +0330

    test(shop/options): test create option item and test create an existing option item
    version: 0.5.0-alphaK

--- apps/shop/demo/factory/option/option_factory.py
+++ apps/shop/demo/factory/option/option_factory.py
@@ -5,34 +5,18 @@ from apps.shop.models.option import Option, OptionItem
 
 class OptionFactory:
     faker = Faker()
-
-    @staticmethod
-    def create_option():
-        return Option.objects.create(option_name="color")
+    option_name = "color"
+    item_name = "red"
 
     @classmethod
-    def populate_demo_options(cls):
-        cls.add_one_item()
-        cls.add_multiple_items()
+    def create_option(cls):
+        return Option.objects.create(option_name=cls.option_name)
 
     @classmethod
-    def add_one_item(cls, get_item: bool = False, quantity: int = 1):
-        option_name = cls.create_option()
-        option_item = OptionItem.objects.create(
-            option_name=option_name, quantity=quantity
-        )
-        if get_item:
-            return option_name, option_item
-        return option_name
+    def add_one_option_item(cls, option_id):
+        return OptionItem.objects.create(option_id=option_id, item_name=cls.item_name)
 
-    @classmethod
-    def add_multiple_items(cls, get_items: bool = False):
-        option_name = cls.create_option()
-
-        option_items = OptionItem.objects.bulk_create(
-            [OptionItem(option_name=option_name, quantity=1)]
-        )
-
-        if get_items:
-            return option_name, option_items
-        return option_name
+    # @classmethod
+    # def populate_demo_options(cls):
+    #     cls.add_one_option_item()
+    #     cls.add_multiple_items()
--- apps/shop/tests/test_option/test_create_option_items.py
+++ apps/shop/tests/test_option/test_create_option_items.py
@@ -5,8 +5,6 @@ from rest_framework import status
 
 from apps.core.tests.base_test import CoreBaseTestCase
 from apps.shop.demo.factory.option.option_factory import OptionFactory
-from apps.shop.demo.factory.product.product_factory import ProductFactory
-from apps.shop.models import Product
 
 
 class CreateOptionItemsTest(CoreBaseTestCase):
@@ -17,15 +15,15 @@ class CreateOptionItemsTest(CoreBaseTestCase):
         super().setUpTestData()
 
     def setUp(self):
+        self.set_admin_user_authorization()
         self.option = OptionFactory.create_option()
-        self.payload = {"item_name": "red"}
+        self.payload = {"item_name": OptionFactory.item_name}
 
     # ------------------------------
     # --- Test Access Permission ---
     # ------------------------------
 
     def test_create_option_item_by_admin(self):
-        self.set_admin_user_authorization()
         response = self.client.post(
             reverse("option-items-list", kwargs={"option_pk": str(self.option.id)}),
             json.dumps(self.payload),
@@ -58,7 +56,7 @@ class CreateOptionItemsTest(CoreBaseTestCase):
     def test_create_one_option_item(self):
         # request
         response = self.client.post(
-            reverse("option-items-list", kwargs={"option_name": self.option_name}),
+            reverse("option-items-list", kwargs={"option_pk": str(self.option.id)}),
             json.dumps(self.payload),
             content_type="application/json",
         )
@@ -68,141 +66,16 @@ class CreateOptionItemsTest(CoreBaseTestCase):
         expected = response.json()
         self.assertIsInstance(expected["item_name"], str)
 
-        # expected quantity and item_total
-        self.assertEqual(expected["quantity"], 1)
-
     def test_create_one_option_item_if_already_exist(self):
-        option_name, option_item = OptionFactory.add_one_item(get_item=True)
-        response = self.client.post(
-            reverse("option-items-list", kwargs={"option_name": option_name}),
-            json.dumps({"option_name": option_item.option_name, "quantity": 1}),
-            content_type="application/json",
-        )
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        # create an option item
+        OptionFactory.add_one_option_item(self.option.id)
 
-    def test_option_total_price(self):
         # request
-        total_price: float = 0
-        payload = {"option_name": self.option_name, "quantity": 1}
         response = self.client.post(
-            reverse("option-items-list", kwargs={"option_name": self.option_name}),
-            json.dumps(payload),
-            content_type="application/json",
-        )
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        expected = response.json()
-        total_price += expected["item_total"]
-
-        # expected
-        response = self.client.get(
-            reverse("option-detail", kwargs={"pk": self.option_name})
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        expected = response.json()
-        self.assertAlmostEqual(expected["total_price"], round(total_price, 2), places=2)
-
-    # --------------------------------------------
-    # --- Test if product status is not active ---
-    # --------------------------------------------
-
-    def test_create_option_item_with_draft_product(self):
-        product = ProductFactory.create_product(status=Product.STATUS_DRAFT)
-        product_variant = product.variants.first()
-        response = self.client.post(
-            reverse(
-                "option-items-list",
-                kwargs={"option_name": self.option_name},
-            ),
-            json.dumps({"variant": product_variant.id, "quantity": 1}),
+            reverse("option-items-list", kwargs={"option_pk": str(self.option.id)}),
+            json.dumps(self.payload),
             content_type="application/json",
         )
 
         # expected
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-
-    def test_create_option_item_with_archived_product(self):
-        product = ProductFactory.create_product(status=Product.STATUS_ARCHIVED)
-        response = self.client.post(
-            reverse(
-                "option-items-list",
-                kwargs={"option_name": self.option_name},
-            ),
-            json.dumps({"quantity": 1}),
-            content_type="application/json",
-        )
-
-        # expected
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-
-    # ---------------------------------------------
-    # --- Test if variant stock is out of stock ---
-    # ---------------------------------------------
-
-    def test_create_option_item_if_variant_not_in_stock(self):
-        product = ProductFactory.create_product(stock=0)
-        response = self.client.post(
-            reverse(
-                "option-items-list",
-                kwargs={"option_name": self.option_name},
-            ),
-            json.dumps({"quantity": 1}),
-            content_type="application/json",
-        )
-
-        # expected
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-
-    def test_create_option_item_quantity_bigger_than_stock(self):
-        product = ProductFactory.create_product(stock=3)
-        response = self.client.post(
-            reverse(
-                "option-items-list",
-                kwargs={"option_name": self.option_name},
-            ),
-            json.dumps({"quantity": 4}),
-            content_type="application/json",
-        )
-
-        # expected
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-
-    # -----------------------
-    # --- Invalid Payload ---
-    # -----------------------
-
-    def test_create_option_item_without_image(self):
-        product = ProductFactory.create_product()
-        response = self.client.post(
-            reverse(
-                "option-items-list",
-                kwargs={"option_name": self.option_name},
-            ),
-            json.dumps({"quantity": 1}),
-            content_type="application/json",
-        )
-
-        # expected
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
-    def test_create_option_item_invalid_quantity(self):
-        invalid_payloads = [
-            {"quantity": -1},
-            {"quantity": 0},
-            {"quantity": "0"},
-            {"quantity": None},
-            {"quantity": True},
-            {"quantity": []},
-            {"quantity": ""},
-            {"variant": 1},
-            {},
-        ]
-        for invalid_payload in invalid_payloads:
-            response = self.client.post(
-                reverse(
-                    "option-items-list",
-                    kwargs={"option_name": self.option_name},
-                ),
-                json.dumps(invalid_payload),
-                content_type="application/json",
-            )
-            self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
