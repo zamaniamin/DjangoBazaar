0b973d1d3f03a47be664d0131fe34e74f2085ea6
AminZamani <aminzamani.work@gmail.com>
2024-06-14 15:25:41 +0330

    test(shop/options): refactor update items
    version: 0.5.3-alphaK

--- apps/shop/tests/test_option/test_option_items/test_update_option_item.py
+++ apps/shop/tests/test_option/test_option_items/test_update_option_item.py
@@ -21,7 +21,7 @@ class UpdateOptionItemTest(CoreBaseTestCase):
     # --- Test Access Permissions ---
     # -------------------------------
 
-    def test_update_option_by_admin(self):
+    def test_update_item_by_admin(self):
         payload = {"item_name": "color"}
         response = self.client.put(
             reverse(
@@ -33,37 +33,38 @@ class UpdateOptionItemTest(CoreBaseTestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_update_option_by_regular_user(self):
+    def test_update_item_by_regular_user(self):
         self.set_regular_user_authorization()
-        payload = {"item_name": "color"}
         response = self.client.put(
             reverse(
                 "option-items-detail",
                 kwargs={"option_pk": self.option.id, "pk": self.item.id},
             ),
-            json.dumps(payload),
             content_type="application/json",
         )
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-    def test_update_option_by_anonymous_user(self):
+    def test_update_item_by_anonymous_user(self):
         self.set_anonymous_user_authorization()
         response = self.client.put(
             reverse(
                 "option-items-detail",
                 kwargs={"option_pk": self.option.id, "pk": self.item.id},
             ),
-            json.dumps({"item_name": "color"}),
             content_type="application/json",
         )
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
-    def test_update_option(self):
-        # get old option name
+    # -------------------------
+    # --- Test Update Items ---
+    # -------------------------
+
+    def test_update_item(self):
+        # get old item name
         old_item_name = self.item.item_name
 
         # request
-        new_item_name = "color2"
+        new_item_name = "red 2"
         payload = {"item_name": new_item_name}
         response = self.client.put(
             reverse(
@@ -87,16 +88,13 @@ class UpdateOptionItemTest(CoreBaseTestCase):
         self.assertEqual(expected["item_name"], new_item_name)
         self.assertNotEqual(old_item_name, new_item_name)
 
-    def test_update_option_item_404(self):
+    def test_update_with_invalid_item_pk(self):
         # request
-        new_item_name = "color2"
-        payload = {"item_name": new_item_name}
         response = self.client.put(
             reverse(
                 "option-items-detail",
                 kwargs={"option_pk": self.option.id, "pk": 999},
             ),
-            json.dumps(payload),
             content_type="application/json",
         )
 
@@ -109,7 +107,6 @@ class UpdateOptionItemTest(CoreBaseTestCase):
                 "option-items-detail",
                 kwargs={"option_pk": 99999, "pk": self.item.id},
             ),
-            json.dumps({"item_name": "blue"}),
             content_type="application/json",
         )
         # expected
