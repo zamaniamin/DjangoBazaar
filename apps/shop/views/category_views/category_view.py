e44bd3701dfb62221cbbf97e7032278fb30cf3e4
AminZamani <aminzamani.work@gmail.com>
2024-06-15 13:01:52 +0330

    docs(shop/categories): add category CRUD docs
    version: 0.6.0-alphaK

--- apps/shop/views/category_views/category_view.py
+++ apps/shop/views/category_views/category_view.py
@@ -1,9 +1,20 @@
+from drf_spectacular.utils import extend_schema_view, extend_schema
 from rest_framework import viewsets
 
 from apps.shop.models.category import Category
 from apps.shop.serializers.category_serializers import CategorySerializer
 
 
+@extend_schema_view(
+    create=extend_schema(tags=["Category"], summary="Create a new category"),
+    retrieve=extend_schema(tags=["Category"], summary="Retrieve a category"),
+    list=extend_schema(tags=["Category"], summary="Retrieve a list of categories"),
+    update=extend_schema(tags=["Category"], summary="Update a category"),
+    partial_update=extend_schema(
+        tags=["Category"], summary="Partial update a category"
+    ),
+    destroy=extend_schema(tags=["Category"], summary="Deletes a category"),
+)
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
