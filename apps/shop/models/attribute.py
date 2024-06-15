d81bbc68fc3db75d759bf4f7f9830a0ed93a0b6f
SEVDA <lostangel_2000@outlook.com>
2024-06-15 17:16:00 +0330

    feat(model/shop):add attribute models

---
+++ apps/shop/models/attribute.py
@@ -0,0 +1,27 @@
+from django.db import models
+    
+
+class Attribute(models.Model):
+
+    class Meta:
+        ordering = ['name']
+
+    name = models.CharField(
+        verbose_name='Name',
+        max_length=100,
+        help_text='User attribute name',
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class AttributeItem(models.Model):
+    attribute = models.ForeignKey(Attribute, on_delete=models.CASCADE, related_name="items")
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        unique_together = ("attribute", "name")
+
+    def __str__(self):
+        return self.name
