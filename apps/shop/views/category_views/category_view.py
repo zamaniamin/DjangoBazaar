9150e0b6614245533fce500a3a80d138608711c7
AminZamani <aminzamani.work@gmail.com>
2024-06-16 18:31:46 +0330

    feat(shop/categories): add pagination
    version: 0.6.5-alpha

--- apps/shop/views/category_views/category_view.py
+++ apps/shop/views/category_views/category_view.py
@@ -4,6 +4,7 @@ from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.permissions import IsAdminUser, AllowAny
 
 from apps.shop.models.category import Category
+from apps.shop.paginations import DefaultPagination
 from apps.shop.serializers.category_serializers import CategorySerializer
 
 
@@ -18,10 +19,11 @@ from apps.shop.serializers.category_serializers import CategorySerializer
     destroy=extend_schema(tags=["Category"], summary="Deletes a category"),
 )
 class CategoryViewSet(viewsets.ModelViewSet):
-    queryset = Category.objects.all()
+    queryset = Category.objects.all().order_by('created_at')
     serializer_class = CategorySerializer
     permission_classes = [IsAdminUser]
     parser_classes = [MultiPartParser, FormParser]
+    pagination_class = DefaultPagination
 
     ACTION_PERMISSIONS = {
         "list": [AllowAny()],
