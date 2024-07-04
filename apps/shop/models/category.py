adfa9bf942a47f412adf34213968386a76937aee
AminZamani <aminzamani.work@gmail.com>
2024-07-04 20:58:55 +0330

    chore(shop/categories): add todo

--- apps/shop/models/category.py
+++ apps/shop/models/category.py
@@ -34,6 +34,7 @@ class Category(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def automatic_slug_creation(self):
+        # TODO fix bug on automatic assign count
         if not self.slug:
             base_slug = slugify(self.name, allow_unicode=True)
             slug = base_slug
