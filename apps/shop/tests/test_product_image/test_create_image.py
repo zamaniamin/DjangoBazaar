beff62ebecb43543fc9ea7fbce494df091dad902
AminZamani <aminzamani.work@gmail.com>
2024-12-21 01:29:41 +0330

    test(shop/products): when create a new main image for product if there was a main image
    version: 0.6.39-alpha

--- apps/shop/tests/test_product_image/test_create_image.py
+++ apps/shop/tests/test_product_image/test_create_image.py
@@ -198,6 +198,40 @@ class ProductImageUploadTest(ProductBaseTestCase):
             file_path = os.path.abspath(str(settings.BASE_DIR) + image["src"])
             self.assertTrue(os.path.exists(file_path))
 
+    def test_upload_one_main_image_with_existing_main_image(self):
+        """test upload a main image for product, if there was a main image befor"""
+        # request, upload a main image
+        payload = {"images": self.files[1], "is_main": True}
+        response = self.post_multipart(
+            reverse(
+                "product-images-list", kwargs={"product_pk": self.active_product.id}
+            ),
+            payload,
+        )
+
+        # expected
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        expected = response.json()
+        old_main_image_id = expected[0]["id"]
+
+        # request, upload second main image
+        payload = {"images": self.files[0], "is_main": True}
+        response2 = self.post_multipart(
+            reverse(
+                "product-images-list", kwargs={"product_pk": self.active_product.id}
+            ),
+            payload,
+        )
+        expected2 = response2.json()
+        self.assertIsInstance(expected2, list)
+        self.assertEqual(len(expected2), 2)
+
+        for image in expected2:
+            if image["id"] == old_main_image_id:
+                self.assertFalse(image["is_main"])
+            else:
+                self.assertTrue(image["is_main"])
+
 
 # TODO test update image
 # TODO test delete image
