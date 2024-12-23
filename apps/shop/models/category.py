03c6391904261304fa73f80679bf63c383138e06
AminZamani <aminzamani.work@gmail.com>
2024-12-23 07:29:08 +0330

    fix(shop/categories): add CategoryImage model
    version: 0.6.40-alpha

--- apps/shop/models/category.py
+++ apps/shop/models/category.py
@@ -1,37 +1,20 @@
-import os
-import sys
-import uuid
-
 from django.db import models
 from django.utils.text import slugify
 
-
-def generate_upload_path(instance, filename):
-    # TODO remove privacy data from image files and only accept jpeg and png formats?
-    unique_id = uuid.uuid4().hex
-    _, ext = os.path.splitext(filename)
-
-    # Add "test_" prefix to unique_id if running in test mode
-    if "test" in sys.argv:
-        return f"test/categories/{unique_id}{ext}"
-    else:
-        return f"categories/{unique_id}{ext}"
+from apps.core.models.image import AbstractImage
+from apps.core.models.timestamped import TimestampedModel
 
 
-class Category(models.Model):
+class Category(TimestampedModel):
     name = models.CharField(max_length=255, unique=True)
     slug = models.SlugField(max_length=255, unique=True, blank=True, allow_unicode=True)
     description = models.TextField(null=True, blank=True)
-    image = models.ImageField(upload_to=generate_upload_path, blank=True, null=True)
     parent = models.ForeignKey(
         "self",
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
     )
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def automatic_slug_creation(self):
         # TODO fix bug on automatic assign count
@@ -47,3 +30,15 @@ class Category(models.Model):
     def save(self, *args, **kwargs):
         self.automatic_slug_creation()
         super().save(*args, **kwargs)
+
+
+class CategoryImage(AbstractImage):
+    category = models.OneToOneField(
+        Category, on_delete=models.CASCADE, related_name="image"
+    )
+
+    def get_related_id(self):
+        return self.category.id
+
+    def get_related_folder(self):
+        return "categories"
