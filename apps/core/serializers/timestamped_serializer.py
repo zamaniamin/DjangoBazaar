f5776459783de7593c741930d8dd91840a8b2ea2
AminZamani <aminzamani.work@gmail.com>
2024-12-23 07:25:10 +0330

    refactor(core/serializers): add TimestampedSerializer
    version: 0.6.39-alpha

---
+++ apps/core/serializers/timestamped_serializer.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+
+class TimestampedSerializer(serializers.ModelSerializer):
+    created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True)
+    updated_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True)
+
+    class Meta:
+        abstract = True
