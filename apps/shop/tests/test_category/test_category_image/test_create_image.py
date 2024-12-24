8290a10257ef6996ba156ec8a76a3d4fbe465b14
AminZamani <aminzamani.work@gmail.com>
2024-12-24 03:49:45 +0330

    test(shop/categories): refactor 'upload' image
    version: 0.6.40-alpha

--- apps/shop/tests/test_category/test_category_image/test_create_image.py
+++ apps/shop/tests/test_category/test_category_image/test_create_image.py
@@ -15,28 +15,44 @@ class CategoryImageUploadTest(CoreBaseTestCase):
         self.set_admin_user_authorization()
         self.category = CategoryFactory.create_category()
 
+    # ----------------------
+    # --- Helper Methods ---
+    # ----------------------
+
+    def upload_image(self, payload):
+        """Helper method to upload an image and return the response"""
+        return self.post_multipart(
+            reverse("category-image-list", kwargs={"category_pk": self.category.id}),
+            payload,
+        )
+
+    def validate_image_response(self, response, payload: dict = None):
+        """Helper method to validate the image response."""
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        expected = response.json()
+        self.assertIsInstance(expected, dict)
+        self.assertEqual(len(expected), 6)
+        self.assertIsInstance(expected["id"], int)
+        self.assertEqual(expected["category_id"], self.category.id)
+        self.assertImageSrcPattern(expected["src"])
+        self.assertEqual(expected["alt"], payload["alt"] if payload else None)
+        self.assertDatetimeFormat(expected["updated_at"])
+        self.assertDatetimeFormat(expected["created_at"])
+        self.assertImageFileDirectory(expected["src"])
+
     # -------------------------------
     # --- Test Access Permissions ---
     # -------------------------------
 
     def test_upload_by_regular_user(self):
         self.set_regular_user_authorization()
-        response = self.post_multipart(
-            reverse(
-                "category-image-list",
-                kwargs={"category_pk": self.category.id},
-            )
-        )
+        response = self.upload_image({})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_upload_by_anonymous_user(self):
         self.set_anonymous_user_authorization()
-        response = self.post_multipart(
-            reverse(
-                "category-image-list",
-                kwargs={"category_pk": self.category.id},
-            )
-        )
+        response = self.upload_image({})
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     # -------------------------
@@ -44,56 +60,19 @@ class CategoryImageUploadTest(CoreBaseTestCase):
     # -------------------------
 
     def test_upload_image(self):
-        # request
         payload = {"src": self.image_file}
-        response = self.post_multipart(
-            reverse("category-image-list", kwargs={"category_pk": self.category.id}),
-            payload,
-        )
-
-        # expected
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        expected = response.json()
-        self.assertIsInstance(expected, dict)
-        self.assertEqual(len(expected), 6)
-        self.assertIsInstance(expected["id"], int)
-        self.assertEqual(expected["category_id"], self.category.id)
-        self.assertImageSrcPattern(expected["src"])
-        self.assertIsNone(expected["alt"])
-        self.assertDatetimeFormat(expected["updated_at"])
-        self.assertDatetimeFormat(expected["created_at"])
-        self.assertImageFileDirectory(expected["src"])
+        response = self.upload_image(payload)
+        self.validate_image_response(response)
 
     def test_upload_image_with_alt(self):
-        # request
         payload = {
             "src": self.image_file,
             "alt": "test alt",
         }
-        response = self.post_multipart(
-            reverse("category-image-list", kwargs={"category_pk": self.category.id}),
-            payload,
-        )
-
-        # expected
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        expected = response.json()
-        self.assertIsInstance(expected, dict)
-        self.assertEqual(len(expected), 6)
-        self.assertIsInstance(expected["id"], int)
-        self.assertEqual(expected["category_id"], self.category.id)
-        self.assertImageSrcPattern(expected["src"])
-        self.assertEqual(expected["alt"], payload["alt"])
-        self.assertDatetimeFormat(expected["updated_at"])
-        self.assertDatetimeFormat(expected["created_at"])
-        self.assertImageFileDirectory(expected["src"])
+        response = self.upload_image(payload)
+        self.validate_image_response(response, payload)
 
     def test_upload_with_empty_src(self):
         payload = {"src": ""}
-        response = self.post_multipart(
-            reverse("category-image-list", kwargs={"category_pk": self.category.id}),
-            payload,
-        )
-
-        # expected
+        response = self.upload_image(payload)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
