58420f319a7c4283d1e7773854a223d0b67776ff
AminZamani <aminzamani.work@gmail.com>
2024-12-24 04:03:11 +0330

    test(shop/categories): refactor 'delete' test case
    version: 0.6.40-alpha

--- apps/shop/tests/test_category/test_destroy_category.py
+++ apps/shop/tests/test_category/test_destroy_category.py
@@ -6,58 +6,60 @@ from apps.shop.demo.factory.category.category_factory import CategoryFactory
 
 
 class DestroyCategoryTest(CoreBaseTestCase):
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.view_name = "category-detail"
+
     def setUp(self):
         self.set_admin_user_authorization()
         self.category = CategoryFactory.create_category()
 
+    # ----------------------
+    # --- Helper Methods ---
+    # ----------------------
+
+    def delete_category(self):
+        """Helper method to delete category and return the response"""
+        return self.client.delete(
+            reverse(self.view_name, kwargs={"pk": self.category.id})
+        )
+
     # -------------------------------
     # --- Test Access Permissions ---
     # -------------------------------
-    def test_delete_category_by_admin(self):
-        response = self.client.delete(
-            reverse("category-detail", kwargs={"pk": self.category.id})
-        )
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
-    def test_delete_category_by_regular_user(self):
+    def test_delete_by_regular_user(self):
         self.set_regular_user_authorization()
-        response = self.client.delete(
-            reverse("category-detail", kwargs={"pk": self.category.id})
-        )
+        response = self.delete_category()
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-    def test_delete_category_by_anonymous_user(self):
+    def test_delete_by_anonymous_user(self):
         self.set_anonymous_user_authorization()
-        response = self.client.delete(
-            reverse("category-detail", kwargs={"pk": self.category.id})
-        )
+        response = self.delete_category()
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     # ----------------------------
     # --- Test Delete Category ---
     # ----------------------------
 
-    def test_delete_category(self):
+    def test_delete(self):
         # request for delete an category
-        response = self.client.delete(
-            reverse("category-detail", kwargs={"pk": self.category.id})
-        )
+        response = self.delete_category()
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
         # assert category is removed
         response = self.client.get(
-            reverse("category-detail", kwargs={"pk": self.category.id})
+            reverse(self.view_name, kwargs={"pk": self.category.id})
         )
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-    def test_delete_category_if_not_exist(self):
-        response = self.client.delete(reverse("category-detail", kwargs={"pk": 999}))
+    def test_delete_if_not_exist(self):
+        response = self.client.delete(reverse(self.view_name, kwargs={"pk": 999}))
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-    def test_delete_parent_category(self):
-        """
-        test destroy category and check the parent field of children is set to null
-        """
+    def test_delete_parent(self):
+        """test destroy category and check the parent field of children is set to null"""
 
         # make a family
         parent = self.category
@@ -68,9 +70,7 @@ class DestroyCategoryTest(CoreBaseTestCase):
         child_2.save()
 
         # request
-        response = self.client.delete(
-            reverse("category-detail", kwargs={"pk": parent.id})
-        )
+        response = self.client.delete(reverse(self.view_name, kwargs={"pk": parent.id}))
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
         # Reload the children from the database to get the latest state
