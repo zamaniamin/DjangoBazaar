4bedef3a5eb1a97147a3c1d081a9488564e6fcfe
AminZamani <aminzamani.work@gmail.com>
2025-01-27 00:34:04 +0330

    test(shop/product): separate list and retrieve test case
    version: 0.6.47-alpha

--- apps/shop/tests/test_product/test_list_product.py
+++ apps/shop/tests/test_product/test_list_product.py
@@ -67,6 +67,8 @@ class ListProductsTest(ProductBaseTestCase):
                     "status",
                     "options",
                     "variants",
+                    "price",
+                    "total_stock",
                     "images",
                     "created_at",
                     "updated_at",
@@ -108,6 +110,8 @@ class ListProductsTest(ProductBaseTestCase):
                     "status",
                     "options",
                     "variants",
+                    "price",
+                    "total_stock",
                     "images",
                     "created_at",
                     "updated_at",
@@ -164,7 +168,7 @@ class ListProductsTest(ProductBaseTestCase):
         expected_products = expected["results"]
         self.assertEqual(len(expected_products), 4)
         for product in expected_products:
-            self.assertEqual(len(product), 11)
+            self.assertEqual(len(product), 13)
             self.assertIn("id", product)
             self.assertIn("name", product)
             self.assertIn("slug", product)
@@ -218,187 +222,6 @@ class ListDraftProductsTest(APITestCase):
         self.assertEqual(expected["count"], 0)
         expected_products = expected["results"]
         self.assertEqual(len(expected_products), 0)
-
-
-class RetrieveProductTest(ProductBaseTestCase):
-    @classmethod
-    def setUpTestData(cls):
-        super().setUpTestData()
-
-        # create product
-        (
-            cls.simple_product_payload,
-            cls.simple_product,
-        ) = ProductFactory.create_product(get_payload=True)
-        (
-            cls.variable_product_payload,
-            cls.variable_product,
-        ) = ProductFactory.create_product(get_payload=True, is_variable=True)
-
-        # products with different status
-        cls.active_product = ProductFactory.create_product()
-        cls.archived_product = ProductFactory.create_product(
-            status=Product.STATUS_ARCHIVED
-        )
-        cls.draft_product = ProductFactory.create_product(status=Product.STATUS_DRAFT)
-
-    # ------------------------------
-    # --- Test Access Permission ---
-    # ------------------------------
-
-    def test_retrieve_product_by_admin(self):
-        """
-        Test case to retrieve product details by an admin user.
-
-        The test sets the admin user's credentials and then sends GET requests for product details
-        for each of the active, archived, and draft products. It asserts that the response status code
-        is HTTP 200 OK for each request.
-        """
-        self.set_admin_user_authorization()
-
-        for product in [self.active_product, self.archived_product, self.draft_product]:
-            # request
-            response = self.client.get(
-                reverse("product-detail", kwargs={"pk": product.id})
-            )
-
-            # expected
-            self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_retrieve_product_by_regular_user(self):
-        """
-        Test case to retrieve product details by a regular user.
-
-        The test sets the regular user's credentials and then sends GET requests for product details
-        for each of the active, archived, and draft products. It asserts that the response status code is
-        HTTP 200 OK for active and archived products, and HTTP 404 Not Found for draft products.
-        """
-        self.set_regular_user_authorization()
-
-        for product in [self.active_product, self.archived_product, self.draft_product]:
-            # request
-            response = self.client.get(
-                reverse("product-detail", kwargs={"pk": product.id})
-            )
-
-            # expected
-            if product.status in [Product.STATUS_ACTIVE, Product.STATUS_ARCHIVED]:
-                self.assertEqual(response.status_code, status.HTTP_200_OK)
-            elif product.status == Product.STATUS_DRAFT:
-                self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-
-    def test_retrieve_product_by_anonymous_user(self):
-        """
-        Test case to retrieve product details by a guest user.
-
-        The test resets the client's credentials, simulating a guest user, and then sends GET requests
-        for product details for each of the active, archived, and draft products. It asserts that the
-        response status code is HTTP 200 OK for active and archived products, and HTTP 404 Not Found for draft products.
-        """
-        self.set_anonymous_user_authorization()
-
-        for product in [self.active_product, self.archived_product, self.draft_product]:
-            # request
-            response = self.client.get(
-                reverse("product-detail", kwargs={"pk": product.id})
-            )
-
-            # expected
-            if product.status in [Product.STATUS_ACTIVE, Product.STATUS_ARCHIVED]:
-                self.assertEqual(response.status_code, status.HTTP_200_OK)
-            elif product.status == Product.STATUS_DRAFT:
-                self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-
-    # -------------------------------
-    # --- Test Retrieve A Product ---
-    # -------------------------------
-
-    def test_retrieve_product(self):
-        """
-        Test retrieve a product:
-        - with product fields.
-        - with one variant.
-        - no options.
-        - no media.
-        """
-
-        # request
-        response = self.client.get(
-            reverse("product-detail", kwargs={"pk": self.simple_product.id})
-        )
-
-        # expected
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        expected = response.json()
-        self.assertIsInstance(expected["id"], int)
-        self.assertEqual(expected["name"], self.simple_product_payload["name"])
-        self.assertEqual(
-            expected["description"], self.simple_product_payload["description"]
-        )
-        self.assertEqual(expected["status"], self.simple_product_payload["status"])
-
-        # expected product date and time
-        self.assertExpectedDatetimeFormat(expected)
-
-        # expected product options
-        self.assertIsNone(expected["options"])
-
-        # expected product variants
-        self.assertEqual(len(expected["variants"]), 1)
-        self.assertExpectedVariants(
-            expected["variants"],
-            expected_price=self.simple_product_payload["price"],
-            expected_stock=self.simple_product_payload["stock"],
-        )
-
-        # TODO add media assertion
-
-    def test_retrieve_product_with_options(self):
-        """
-        Test retrieve a product with options:
-        - with price and stock.
-        - with options
-        - with variants.
-        - no media.
-        """
-
-        # request
-        response = self.client.get(
-            reverse("product-detail", kwargs={"pk": self.variable_product.id})
-        )
-
-        # expected product
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        expected = response.json()
-        self.assertIsInstance(expected["id"], int)
-        self.assertEqual(expected["name"], self.variable_product_payload["name"])
-        self.assertEqual(
-            expected["description"], self.variable_product_payload["description"]
-        )
-        self.assertEqual(expected["status"], self.variable_product_payload["status"])
-
-        # expected product date and time
-        self.assertExpectedDatetimeFormat(expected)
-
-        # expected product options
-        self.assertEqual(len(expected["options"]), 3)
-        self.assertExpectedOptions(
-            expected["options"], self.variable_product_payload["options"]
-        )
-
-        # expected product variants
-        self.assertTrue(len(expected["variants"]) == 8)
-        self.assertExpectedVariants(
-            expected["variants"],
-            self.variable_product_payload["price"],
-            self.variable_product_payload["stock"],
-        )
-
-        # TODO add media assertion
-
-    def test_retrieve_product_if_not_exist(self):
-        response = self.client.get(reverse("product-detail", kwargs={"pk": 999}))
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
 
 # TODO test with images
---
+++ apps/shop/tests/test_product/test_retrieve_variable_product.py
@@ -0,0 +1,81 @@
+from django.urls import reverse
+from rest_framework import status
+
+from apps.shop.demo.factory.product.product_factory import ProductFactory
+from apps.shop.tests.test_product.base_test_case import ProductBaseTestCase
+
+
+class RetrieveVariableProductTest(ProductBaseTestCase):
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+
+        # create variable product
+        cls.variable_product_payload, cls.variable_product = (
+            ProductFactory.create_product(get_payload=True, is_variable=True)
+        )
+
+    # ----------------------
+    # --- Helper Methods ---
+    # ----------------------
+
+    def validate_response(self, response, payload):
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        expected = response.json()
+        self.validate_product_general_data(expected)
+        self.validate_product_options(expected["options"])
+        self.validate_product_variants(expected["variants"])
+
+    def validate_product_general_data(self, expected):
+        self.assertEqual(len(expected), 13)
+        self.assertIsInstance(expected["id"], int)
+        self.assertEqual(expected["name"], self.variable_product_payload["name"])
+
+        # TODO add slug
+        self.assertEqual(
+            expected["description"], self.variable_product_payload["description"]
+        )
+        self.assertEqual(expected["status"], self.variable_product_payload["status"])
+
+        # expected product date and time
+        self.assertExpectedDatetimeFormat(expected)
+
+        self.assertEqual(
+            set(expected["price"].keys()),
+            {"min_price", "max_price"},
+        )
+
+        self.assertIsInstance(expected["total_stock"], int)
+        # TODO add min price
+        # TODO add max price
+        # TODO add total stock
+
+    def validate_product_options(self, options):
+        # expected product options
+        self.assertEqual(len(options), 3)
+
+    def validate_product_variants(self, variants):
+        # expected product variants
+        self.assertEqual(len(variants), 8)
+        self.assertExpectedVariants(
+            variants,
+            expected_price=self.variable_product_payload["price"],
+            expected_stock=self.variable_product_payload["stock"],
+        )
+
+    # ------------------------------
+    # --- Test Access Permission ---
+    # ------------------------------
+    # TODO add access permission tests
+
+    def test_retrieve(self):
+        # request
+        response = self.client.get(
+            reverse("product-detail", kwargs={"pk": self.variable_product.id})
+        )
+        self.validate_response(response, self.variable_product_payload)
+
+    # todo test_retrieve_with_min_price
+    # todo test_retrieve_with_max_price
+    # todo test_retrieve_with_total_stock
