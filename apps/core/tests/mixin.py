9a141b9ceed1504311a576be303ea730f0bc9a99
AminZamani <aminzamani.work@gmail.com>
2025-01-28 02:08:47 +0330

    test(core/test): refactor code structure
    version: 0.6.48-alpha

--- apps/core/tests/mixin.py
+++ apps/core/tests/mixin.py
@@ -2,10 +2,11 @@ import io
 import json
 import os
 import re
-from abc import ABC
+from abc import ABC, abstractmethod
 from datetime import datetime
 
 from PIL import Image
+from rest_framework import status
 from rest_framework.test import APITestCase
 
 from apps.core.demo.factory.user_factory import UserFactory
@@ -14,7 +15,7 @@ from apps.core.services.token_service import TokenService
 from config import settings
 
 
-class APITestCaseMixin(ABC, APITestCase):
+class _APITestCaseMixin_(APITestCase):
     @classmethod
     def setUpTestData(cls):
         # Runs once per test class.
@@ -28,22 +29,6 @@ class APITestCaseMixin(ABC, APITestCase):
             cls.regular_user
         )
 
-    def send_request(self, *args, **kwargs):
-        pass
-
-    # def setUp(self):
-    # Runs before every test method.
-
-    # The reason you cannot call `set_admin_user_authorization()` in the `setUpTestData()` method is that
-    # `setUpTestData()` is a class method and thus operates at the class level, not the instance level.
-    # The `self` parameter in `set_admin_user_authorization()` refers to an instance of the test case, which is not
-    # available within a class method.
-
-    # In `setUpTestData()`, you only have access to `cls`, which is the class itself, not an instance.
-    # Instance methods (like `set_admin_user_authorization`) require an instance of the class to be called.
-
-    # self.set_admin_user_authorization()
-
     def set_admin_user_authorization(self):
         self.client.credentials(HTTP_AUTHORIZATION=f"JWT {self.admin_access_token}")
 
@@ -54,16 +39,6 @@ class APITestCaseMixin(ABC, APITestCase):
 
     def set_anonymous_user_authorization(self):
         self.client.credentials()
-
-    @staticmethod
-    def assertDatetimeFormat(date: str | datetime):
-        if isinstance(date, datetime):
-            date = DateTime.string(date)
-
-        formatted_date = datetime.strptime(date, "%Y-%m-%d %H:%M:%S").strftime(
-            "%Y-%m-%d %H:%M:%S"
-        )
-        assert date == formatted_date
 
     def assertImageSrcPattern(self, src):
         # Define the regex pattern for the src
@@ -131,3 +106,88 @@ class APITestCaseMixin(ABC, APITestCase):
         return self.client.put(
             url, data=json.dumps(data), content_type="application/json", **kwargs
         )
+
+
+class _APITestCaseAuthorizationMixin(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Runs once per test class.
+
+        # create users
+        cls.admin = UserFactory.create(is_staff=True)
+        cls.admin_access_token = TokenService.jwt_get_access_token(cls.admin)
+
+        cls.regular_user = UserFactory.create()
+        cls.regular_user_access_token = TokenService.jwt_get_access_token(
+            cls.regular_user
+        )
+
+    def authorization_as_admin_user(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f"JWT {self.admin_access_token}")
+
+    def authorization_as_regular_user(self):
+        self.client.credentials(
+            HTTP_AUTHORIZATION=f"JWT {self.regular_user_access_token}"
+        )
+
+    def authorization_as_anonymous_user(self):
+        self.client.credentials()
+
+
+class APIAssertMixin(APITestCase):
+    @staticmethod
+    def assertDatetimeFormat(date: str | datetime):
+        if isinstance(date, datetime):
+            date = DateTime.string(date)
+
+        formatted_date = datetime.strptime(date, "%Y-%m-%d %H:%M:%S").strftime(
+            "%Y-%m-%d %H:%M:%S"
+        )
+        assert date == formatted_date
+
+
+class APIPostTestCaseMixin(ABC, _APITestCaseAuthorizationMixin):
+
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.response = None
+
+    def setUp(self):
+        self.authorization_as_admin_user()
+
+    @abstractmethod
+    def api_path(self) -> str:
+        raise NotImplementedError("Please implement`api_path()` in your test class!")
+
+    def send_request(self, payload: dict = None, **kwargs):
+        """Send a POST request to the server and return response."""
+        return self.client.post(
+            path=self.api_path(),
+            data=json.dumps(payload if payload else {}),
+            content_type="application/json",
+            **kwargs,
+        )
+
+    @abstractmethod
+    def validate_response_body(self, response, payload):
+        """Expected response body."""
+        self.response = response.json()
+        self._expected_status_code(response)
+
+    def _expected_status_code(self, response):
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    # ------------------------------
+    # --- Test Access Permission ---
+    # ------------------------------
+
+    def test_access_permission_by_regular_user(self):
+        self.authorization_as_regular_user()
+        response = self.send_request()
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_access_permission_by_anonymous_user(self):
+        self.authorization_as_anonymous_user()
+        response = self.send_request()
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
