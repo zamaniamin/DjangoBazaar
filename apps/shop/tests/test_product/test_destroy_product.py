0848cd6968ab8cca79fb14a7fd170a53bfc05bf4
AminZamani <aminzamani.work@gmail.com>
2025-01-28 18:23:14 +0330

    test(shop/product): refactor delete product test case
    version: 0.6.48-alpha

--- apps/shop/tests/test_product/test_destroy_product.py
+++ apps/shop/tests/test_product/test_destroy_product.py
@@ -1,47 +1,20 @@
 from django.urls import reverse
-from rest_framework import status
 
+from apps.core.tests.mixin import APIDeleteTestCaseMixin
 from apps.shop.demo.factory.product.product_factory import ProductFactory
-from apps.shop.tests.test_product.base_test_case import ProductBaseTestCaseMixin
+from apps.shop.tests.test_product.mixin import _ProductAssertMixin
 
 
-class DestroyProductTest(ProductBaseTestCaseMixin):
-    @classmethod
-    def setUp(cls):
-        cls.product = ProductFactory.create_product()
+class DestroyProductTest(APIDeleteTestCaseMixin, _ProductAssertMixin):
+    def setUp(self):
+        super().setUp()
+        self.product = ProductFactory.create_product()
 
-    # ----------------------
-    # --- Helper Methods ---
-    # ----------------------
-
-    def send_request(self):
-        """Send a DELETE request to the server and return response."""
-        return self.client.delete(
-            reverse("product-detail", kwargs={"pk": self.product.id})
-        )
-
-    # ------------------------------
-    # --- Test Access Permission ---
-    # ------------------------------
-
-    def test_delete_by_regular_user(self):
-        self.set_regular_user_authorization()
-        response = self.send_request()
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-    def test_delete_by_anonymous_user(self):
-        self.set_anonymous_user_authorization()
-        response = self.send_request()
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-
-    # ---------------------------
-    # --- Test Delete Product ---
-    # ---------------------------
+    def api_path(self) -> str:
+        return reverse("product-detail", kwargs={"pk": self.product.id})
 
     def test_delete(self):
-        self.set_admin_user_authorization()
         response = self.send_request()
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-
+        self.expected_status_code(response)
 
 # TODO test destroy a product deletes all related information too
