282f4c208fa78d2381f76bf8a32b3942dcf56660
AminZamani <aminzamani.work@gmail.com>
2025-01-29 20:57:04 +0330

    test(shop/attribute): refactor destroy cart item test case
    version: 0.6.48-alpha

--- apps/shop/tests/test_cart/test_cart_items/test_destroy_cart_item.py
+++ apps/shop/tests/test_cart/test_cart_items/test_destroy_cart_item.py
@@ -1,71 +1,42 @@
 from django.urls import reverse
 from rest_framework import status
 
-from apps.core.tests.mixin import APITestCaseMixin
+from apps.core.tests.mixin import APIDeleteTestCaseMixin
 from apps.shop.demo.factory.cart.cart_factory import CartFactory
 
 
-class DestroyCartItemsTest(APITestCaseMixin):
+class DestroyCartItemsTest(APIDeleteTestCaseMixin):
+
     def setUp(self):
+        super().setUp()
         self.cart_id, self.cart_item = CartFactory.add_one_item(get_item=True)
 
-    # -------------------------------
-    # --- Test Access Permissions ---
-    # -------------------------------
-    def test_delete_item_by_admin(self):
-        self.set_admin_user_authorization()
-        response = self.client.delete(
-            reverse(
-                "cart-items-detail",
-                kwargs={"cart_pk": self.cart_id, "pk": self.cart_item.id},
-            )
+    def api_path(self) -> str:
+        return reverse(
+            "cart-items-detail",
+            kwargs={"cart_pk": self.cart_id, "pk": self.cart_item.id},
         )
+
+    def test_access_permission_by_regular_user(self):
+        self.authorization_as_regular_user()
+        response = self.send_request()
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
-    def test_delete_item_by_regular_user(self):
-        self.set_regular_user_authorization()
-        response = self.client.delete(
-            reverse(
-                "cart-items-detail",
-                kwargs={"cart_pk": self.cart_id, "pk": self.cart_item.id},
-            )
-        )
+    def test_access_permission_by_anonymous_user(self):
+        self.authorization_as_anonymous_user()
+        response = self.send_request()
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
-    def test_delete_item_by_anonymous_user(self):
-        self.set_anonymous_user_authorization()
-        response = self.client.delete(
-            reverse(
-                "cart-items-detail",
-                kwargs={"cart_pk": self.cart_id, "pk": self.cart_item.id},
-            )
-        )
+    def test_delete(self):
+        response = self.send_request()
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
-    # -----------------------------
-    # --- Test Delete Cart Item ---
-    # -----------------------------
-
-    def test_delete_item(self):
-        response = self.client.delete(
-            reverse(
-                "cart-items-detail",
-                kwargs={"cart_pk": self.cart_id, "pk": self.cart_item.id},
-            )
-        )
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-
-        # --- test cart item is removed ---
-        response = self.client.get(
-            reverse(
-                "cart-items-detail",
-                kwargs={"cart_pk": self.cart_id, "pk": self.cart_item.id},
-            )
-        )
+        # test cart item is removed
+        response = self.client.get(self.api_path())
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-    def test_delete_item_with_invalid_cart_pk(self):
-        response = self.client.delete(
+    def test_delete_with_invalid_cart_pk(self):
+        response = self.send_request(
             reverse(
                 "cart-items-detail",
                 kwargs={"cart_pk": 7, "pk": self.cart_item.id},
