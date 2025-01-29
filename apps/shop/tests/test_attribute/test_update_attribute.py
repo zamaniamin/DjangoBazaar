dc356872d8060f04942dc9e600e9805bf177d6e4
AminZamani <aminzamani.work@gmail.com>
2025-01-29 20:32:17 +0330

    test(shop/attribute): refactor update attribute test case
    version: 0.6.48-alpha

--- apps/shop/tests/test_attribute/test_update_attribute.py
+++ apps/shop/tests/test_attribute/test_update_attribute.py
@@ -1,82 +1,24 @@
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.utils import json
 
-from apps.core.tests.mixin import APITestCaseMixin
+from apps.core.tests.mixin import APIUpdateTestCaseMixin
 from apps.shop.demo.factory.attribute.attribute_factory import AttributeFactory
 
 
-class UpdateAttributeTest(APITestCaseMixin):
+class UpdateAttributeTest(APIUpdateTestCaseMixin):
     @classmethod
     def setUpTestData(cls):
         super().setUpTestData()
         cls.attribute = AttributeFactory.create_attribute()
 
-    def setUp(self):
-        self.set_admin_user_authorization()
-
-    # -------------------------------
-    # --- Test Access Permissions ---
-    # -------------------------------
-
-    def test_update_attribute_by_admin(self):
-        # request
-        payload = {"attribute_name": AttributeFactory.attribute_name_2}
-        response = self.client.put(
-            reverse(
-                "attribute-detail",
-                kwargs={"pk": self.attribute.id},
-            ),
-            json.dumps(payload),
-            content_type="application/json",
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_update_attribute_by_regular_user(self):
-        self.set_regular_user_authorization()
-        response = self.client.put(
-            reverse(
-                "attribute-detail",
-                kwargs={"pk": self.attribute.id},
-            ),
-            content_type="application/json",
-        )
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-    def test_update_attribute_by_anonymous_user(self):
-        self.set_anonymous_user_authorization()
-        response = self.client.put(
-            reverse(
-                "attribute-detail",
-                kwargs={"pk": self.attribute.id},
-            ),
-            content_type="application/json",
-        )
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-
-    # ------------------------
-    # --- Update Attribute ---
-    # ------------------------
-
-    def test_update_attribute(self):
-        # get old attribute name
-        old_attribute_name = self.attribute.attribute_name
-
-        # request
-        new_attribute_name = "new attribute"
-        payload = {"attribute_name": new_attribute_name}
-        response = self.client.put(
-            reverse(
-                "attribute-detail",
-                kwargs={"pk": self.attribute.id},
-            ),
-            json.dumps(payload),
-            content_type="application/json",
+    def api_path(self) -> str:
+        return reverse(
+            "attribute-detail",
+            kwargs={"pk": self.attribute.id},
         )
 
-        # expected
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        expected = response.json()
+    def validate_response_body(self, response, payload):
+        super().validate_response_body(response, payload)
         self.assertEqual(
             set(response.data.keys()),
             {
@@ -86,18 +28,21 @@ class UpdateAttributeTest(APITestCaseMixin):
                 "updated_at",
             },
         )
-        self.assertEqual(expected["attribute_name"], new_attribute_name)
-        self.assertNotEqual(old_attribute_name, new_attribute_name)
+        self.assertEqual(self.response["attribute_name"], payload["attribute_name"])
 
-    def test_update_attribute_not_exist(self):
-        # request
-        response = self.client.put(
-            reverse(
-                "attribute-detail",
-                kwargs={"pk": 999},
-            ),
-            content_type="application/json",
+    def test_access_permission_by_regular_user(self):
+        self.check_access_permission_by_regular_user()
+
+    def test_access_permission_by_anonymous_user(self):
+        self.check_access_permission_by_anonymous_user()
+
+    def test_update(self):
+        payload = {"attribute_name": "new attribute"}
+        response = self.send_request(payload)
+        self.validate_response_body(response, payload)
+
+    def test_update_404(self):
+        response = self.send_request(
+            path=reverse("attribute-detail", kwargs={"pk": 999})
         )
-
-        # expected
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
