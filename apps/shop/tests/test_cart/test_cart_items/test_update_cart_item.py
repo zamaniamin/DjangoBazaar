1ea71cd846656299c97e8b7eba072aabb1859771
AminZamani <aminzamani.work@gmail.com>
2025-01-29 21:22:08 +0330

    test(shop/cart): refactor update cart item test case
    version: 0.6.48-alpha

--- apps/shop/tests/test_cart/test_cart_items/test_update_cart_item.py
+++ apps/shop/tests/test_cart/test_cart_items/test_update_cart_item.py
@@ -3,69 +3,65 @@ import json
 from django.urls import reverse
 from rest_framework import status
 
-from apps.core.tests.mixin import APITestCaseMixin
+from apps.core.tests.mixin import APIUpdateTestCaseMixin
 from apps.shop.demo.factory.cart.cart_factory import CartFactory
 
 
-class UpdateCartItemTest(APITestCaseMixin):
+class UpdateCartItemTest(APIUpdateTestCaseMixin):
     @classmethod
     def setUpTestData(cls):
         super().setUpTestData()
         cls.cart_id, cls.cart_item = CartFactory.add_one_item(get_item=True)
 
-    # -------------------------------
-    # --- Test Access Permissions ---
-    # -------------------------------
+    def api_path(self) -> str:
+        return reverse(
+            "cart-items-detail",
+            kwargs={"cart_pk": self.cart_id, "pk": self.cart_item.id},
+        )
 
-    def test_update_item_by_admin(self):
-        self.set_admin_user_authorization()
+    def validate_response_body(self, response, payload):
+        super().validate_response_body(response, payload)
+
+    def test_access_permission_by_regular_user(self):
+        self.authorization_as_regular_user()
+        payload = {"quantity": 3}
         response = self.client.patch(
-            reverse(
-                "cart-items-detail",
-                kwargs={"cart_pk": self.cart_id, "pk": self.cart_item.id},
-            ),
-            json.dumps({"quantity": 3}),
+            self.api_path(),
+            json.dumps(payload),
             content_type="application/json",
         )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.expected_status_code(response)
 
-    def test_update_item_by_regular_user(self):
-        self.set_regular_user_authorization()
+    def test_access_permission_by_anonymous_user(self):
+        self.authorization_as_anonymous_user()
+        payload = {"quantity": 3}
         response = self.client.patch(
-            reverse(
-                "cart-items-detail",
-                kwargs={"cart_pk": self.cart_id, "pk": self.cart_item.id},
-            ),
-            json.dumps({"quantity": 3}),
+            self.api_path(),
+            json.dumps(payload),
             content_type="application/json",
         )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.expected_status_code(response)
 
-    def test_update_item_by_anonymous_user(self):
-        self.set_anonymous_user_authorization()
+    def test_update(self):
+        payload = {"quantity": 3}
         response = self.client.patch(
-            reverse(
-                "cart-items-detail",
-                kwargs={"cart_pk": self.cart_id, "pk": self.cart_item.id},
-            ),
-            json.dumps({"quantity": 3}),
+            self.api_path(),
+            json.dumps(payload),
             content_type="application/json",
         )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.expected_status_code(response)
 
-    def test_update_item_quantity(self):
+    def test_update_quantity(self):
         new_quantity = self.cart_item.quantity + 1
         price = float(self.cart_item.variant.price)
         item_total = price * new_quantity
+        payload = {"quantity": new_quantity}
         response = self.client.patch(
-            reverse(
-                "cart-items-detail",
-                kwargs={"cart_pk": self.cart_id, "pk": self.cart_item.id},
-            ),
-            json.dumps({"quantity": new_quantity}),
+            self.api_path(),
+            json.dumps(payload),
             content_type="application/json",
         )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.expected_status_code(response)
         expected_cart_item = response.json()
         self.assertIn("id", expected_cart_item)
         self.assertIn("variant", expected_cart_item)
@@ -83,37 +79,33 @@ class UpdateCartItemTest(APITestCaseMixin):
             expected_cart_item["item_total"], round(item_total, 2), places=2
         )
 
-    def test_update_item_quantity_bigger_than_stock(self):
+    def test_update_quantity_bigger_than_stock(self):
         cart_id, cart_item = CartFactory.add_one_item(get_item=True, stock=1)
-
+        payload = {"quantity": 3}
         response = self.client.patch(
             reverse(
                 "cart-items-detail",
                 kwargs={"cart_pk": cart_id, "pk": cart_item.id},
             ),
-            json.dumps({"quantity": 3}),
+            json.dumps(payload),
             content_type="application/json",
         )
-
-        # expected
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-    def test_update_item_if_variant_not_in_stock(self):
+    def test_update_if_variant_is_out_of_stock(self):
         cart_id, cart_item = CartFactory.add_one_item(get_item=True, stock=0)
-
+        payload = {"quantity": 3}
         response = self.client.patch(
             reverse(
                 "cart-items-detail",
                 kwargs={"cart_pk": cart_id, "pk": cart_item.id},
             ),
-            json.dumps({"quantity": 3}),
+            json.dumps(payload),
             content_type="application/json",
         )
-
-        # expected
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-    def test_update_item_if_cart_not_exist(self):
+    def test_update_if_cart_not_exist(self):
         cart_id, cart_item = CartFactory.add_one_item(get_item=True)
         response = self.client.patch(
             reverse(
@@ -126,36 +118,33 @@ class UpdateCartItemTest(APITestCaseMixin):
             json.dumps({"quantity": 3}),
             content_type="application/json",
         )
-
-        # expected
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-    def test_update_item_if_not_exist(self):
+    def test_update_if_cart_item_not_exist(self):
         cart_id, cart_item = CartFactory.add_one_item(get_item=True)
+        payload = {"quantity": 3}
         response = self.client.patch(
             reverse(
                 "cart-items-detail",
                 kwargs={"cart_pk": cart_id, "pk": 1111},
             ),
-            json.dumps({"quantity": 3}),
+            json.dumps(payload),
             content_type="application/json",
         )
-
-        # expected
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-    def test_update_item_with_invalid_cart_pk(self):
+    def test_update_with_invalid_cart_pk(self):
         cart_id, cart_item = CartFactory.add_one_item(get_item=True)
+        payload = {"quantity": 3}
         response = self.client.patch(
             reverse(
                 "cart-items-detail",
                 kwargs={"cart_pk": 7, "pk": cart_item.id},
             ),
-            json.dumps({"quantity": 3}),
+            json.dumps(payload),
             content_type="application/json",
         )
-
-        # expected
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
 
 # TODO test update with invalid payload
