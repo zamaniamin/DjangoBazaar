844607efdf92d8edfc9a324ba7aa5184b25a9b8c
AminZamani <aminzamani.work@gmail.com>
2025-01-29 15:33:56 +0330

    test(shop/attribute): refactor destroy attribute item
    version: 0.6.48-alpha

--- apps/shop/tests/test_attribute/test_attribute_items/test_destroy_attribute_items.py
+++ apps/shop/tests/test_attribute/test_attribute_items/test_destroy_attribute_items.py
@@ -5,74 +5,31 @@ from apps.core.tests.mixin import APIDeleteTestCaseMixin
 from apps.shop.demo.factory.attribute.attribute_factory import AttributeFactory
 
 
-class DestroyAttributeItemsTest(APIDeleteTestCaseMixin):
+class DestroyAttributeItemTest(APIDeleteTestCaseMixin):
     @classmethod
     def setUpTestData(cls):
         super().setUpTestData()
         cls.attribute = AttributeFactory.create_attribute()
         cls.attribute_item = AttributeFactory.add_one_attribute_item(cls.attribute.id)
 
-    def setUp(self):
-        self.set_admin_user_authorization()
-
-    # -------------------------------
-    # --- Test Access Permissions ---
-    # -------------------------------
-
-    def test_delete_item_by_admin(self):
-        response = self.client.delete(
-            reverse(
-                "attribute-items-detail",
-                kwargs={
-                    "attribute_pk": self.attribute.id,
-                    "pk": self.attribute_item.id,
-                },
-            )
+    def api_path(self) -> str:
+        return reverse(
+            "attribute-items-detail",
+            kwargs={
+                "attribute_pk": self.attribute.id,
+                "pk": self.attribute_item.id,
+            },
         )
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
-    def test_delete_item_by_regular_user(self):
-        self.set_regular_user_authorization()
-        response = self.client.delete(
-            reverse(
-                "attribute-items-detail",
-                kwargs={
-                    "attribute_pk": self.attribute.id,
-                    "pk": self.attribute_item.id,
-                },
-            )
-        )
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    def test_access_permission_by_regular_user(self):
+        self.check_access_permission_by_regular_user()
 
-    def test_delete_item_by_anonymous_user(self):
-        self.set_anonymous_user_authorization()
-        response = self.client.delete(
-            reverse(
-                "attribute-items-detail",
-                kwargs={
-                    "attribute_pk": self.attribute.id,
-                    "pk": self.attribute_item.id,
-                },
-            )
-        )
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+    def test_access_permission_by_anonymous_user(self):
+        self.check_access_permission_by_anonymous_user()
 
-    # ---------------------------
-    # --- Test Delete An Item ---
-    # ---------------------------
-
-    def test_delete_item(self):
-        # delete an attribute item
-        response = self.client.delete(
-            reverse(
-                "attribute-items-detail",
-                kwargs={
-                    "attribute_pk": self.attribute.id,
-                    "pk": self.attribute_item.id,
-                },
-            )
-        )
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+    def test_delete(self):
+        response = self.send_request()
+        self.expected_status_code(response)
 
         # check that attribute item was deleted
         response = self.client.get(
@@ -86,8 +43,8 @@ class DestroyAttributeItemsTest(APIDeleteTestCaseMixin):
         )
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-    def test_delete_item_if_attribute_not_exist(self):
-        response = self.client.delete(
+    def test_delete_if_attribute_not_exist(self):
+        response = self.send_request(
             reverse(
                 "attribute-items-detail",
                 kwargs={"attribute_pk": 999, "pk": self.attribute_item.id},
@@ -95,8 +52,8 @@ class DestroyAttributeItemsTest(APIDeleteTestCaseMixin):
         )
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-    def test_delete_item_if_item_not_exist(self):
-        response = self.client.delete(
+    def test_delete_if_item_not_exist(self):
+        response = self.send_request(
             reverse(
                 "attribute-items-detail",
                 kwargs={"attribute_pk": self.attribute.id, "pk": 999},
