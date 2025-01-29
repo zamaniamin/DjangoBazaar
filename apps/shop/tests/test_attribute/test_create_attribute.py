f38ea24b32f0fc50954d3b4e5cfb886899a41e51
AminZamani <aminzamani.work@gmail.com>
2025-01-29 18:09:29 +0330

    test(shop/attribute): refactor create attribute test case
    version: 0.6.48-alpha

--- apps/shop/tests/test_attribute/test_create_attribute.py
+++ apps/shop/tests/test_attribute/test_create_attribute.py
@@ -1,86 +1,42 @@
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.utils import json
 
-from apps.core.tests.mixin import APITestCaseMixin
+from apps.core.tests.mixin import APIPostTestCaseMixin
 from apps.shop.demo.factory.attribute.attribute_factory import AttributeFactory
 
 
-class CreateAttributeTest(APITestCaseMixin):
-    def setUp(self):
-        self.set_admin_user_authorization()
+class CreateAttributeTest(APIPostTestCaseMixin):
+    def api_path(self) -> str:
+        return reverse("attribute-list")
 
-    # ------------------------------
-    # --- Test Access Permission ---
-    # ------------------------------
+    def validate_response_body(self, response, payload):
+        super().validate_response_body(response, payload)
+        self.assertEqual(len(self.response), 4)
+        self.assertEqual(self.response["attribute_name"], payload["attribute_name"])
 
-    def test_create_attribute_by_admin(self):
+    def test_access_permission_by_regular_user(self):
+        self.check_access_permission_by_regular_user()
+
+    def test_access_permission_by_anonymous_user(self):
+        self.check_access_permission_by_anonymous_user()
+
+    def test_create(self):
         payload = {
             "attribute_name": "test attribute",
         }
-        response = self.client.post(
-            reverse("attribute-list"),
-            json.dumps(payload),
-            content_type="application/json",
-        )
+        response = self.send_request(payload)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.validate_response_body(response, payload)
 
-    def test_create_attributes_by_regular_user(self):
-        self.set_regular_user_authorization()
-        response = self.client.post(reverse("attribute-list"))
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-    def test_create_attributes_by_anonymous_user(self):
-        self.set_anonymous_user_authorization()
-        response = self.client.post(reverse("attribute-list"))
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-
-    # --------------------------------
-    # --- Test Create An Attribute ---
-    # --------------------------------
-
-    def test_create_attribute(self):
-        # request
-        payload = {
-            "attribute_name": "test attribute",
-        }
-        response = self.client.post(
-            reverse("attribute-list"),
-            json.dumps(payload),
-            content_type="application/json",
-        )
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
-        # expected
-        expected = response.json()
-        self.assertEqual(len(expected), 4)
-        self.assertEqual(expected["attribute_name"], payload["attribute_name"])
-
-    def test_create_attribute_if_already_exist(self):
-        # create an attribute item
+    def test_create_if_already_exist(self):
         AttributeFactory.create_attribute()
-
-        # request
         payload = {
             "attribute_name": AttributeFactory.attribute_name,
         }
-        response = self.client.post(
-            reverse("attribute-list"),
-            json.dumps(payload),
-            content_type="application/json",
-        )
-
-        # expected
+        response = self.send_request(payload)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-    def test_create_attribute_if_name_is_empty(self):
-        # request
+    def test_create_if_name_is_empty(self):
         payload = {"attribute_name": ""}
-        response = self.client.post(
-            reverse("attribute-list"),
-            json.dumps(payload),
-            content_type="application/json",
-        )
-
-        # expected
+        response = self.send_request(payload)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
