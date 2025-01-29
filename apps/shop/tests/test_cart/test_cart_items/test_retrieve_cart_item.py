45abdf97b8aa489dc9bad7c6ddea92ab9d3c4a34
AminZamani <aminzamani.work@gmail.com>
2025-01-29 21:07:35 +0330

    test(shop/cart): refactor retrieve cart item test case
    version: 0.6.48-alpha

--- apps/shop/tests/test_cart/test_cart_items/test_retrieve_cart_item.py
+++ apps/shop/tests/test_cart/test_cart_items/test_retrieve_cart_item.py
@@ -1,57 +1,23 @@
 from django.urls import reverse
 from rest_framework import status
 
-from apps.core.tests.mixin import APITestCaseMixin
+from apps.core.tests.mixin import APIGetTestCaseMixin
 from apps.shop.demo.factory.cart.cart_factory import CartFactory
 
 
-class ListCartItemsTest(APITestCaseMixin):
+class ListCartItemsTest(APIGetTestCaseMixin):
     @classmethod
-    def setUpTestData(self):
+    def setUpTestData(cls):
         super().setUpTestData()
-        self.cart_id, self.cart_items = CartFactory.add_multiple_items(get_items=True)
+        cls.cart_id, cls.cart_items = CartFactory.add_multiple_items(get_items=True)
 
-    # -------------------------------
-    # --- Test Access Permissions ---
-    # -------------------------------
+    def api_path(self) -> str:
+        return reverse("cart-items-list", kwargs={"cart_pk": self.cart_id})
 
-    def test_list_items_by_admin(self):
-        self.set_admin_user_authorization()
-        response = self.client.get(
-            reverse("cart-items-list", kwargs={"cart_pk": self.cart_id})
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_list_items_by_regular_user(self):
-        self.set_regular_user_authorization()
-        response = self.client.get(
-            reverse("cart-items-list", kwargs={"cart_pk": self.cart_id})
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_list_items_by_anonymous_user(self):
-        self.set_anonymous_user_authorization()
-        response = self.client.get(
-            reverse("cart-items-list", kwargs={"cart_pk": self.cart_id})
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    # -----------------------
-    # --- Test List Items ---
-    # -----------------------
-
-    def test_list_items(self):
-        # make request
-        response = self.client.get(
-            reverse("cart-items-list", kwargs={"cart_pk": self.cart_id})
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-        # expected
-        expected_cart_items = response.json()
-        self.assertEqual(len(expected_cart_items), len(self.cart_items))
-
-        for item in expected_cart_items:
+    def validate_response_body(self, response, payload: dict = None):
+        super().validate_response_body(response, payload)
+        self.assertEqual(len(self.response), len(self.cart_items))
+        for item in self.response:
             self.assertIn("id", item)
             self.assertIn("variant", item)
             variant = item["variant"]
@@ -66,78 +32,43 @@ class ListCartItemsTest(APITestCaseMixin):
             self.assertIn("quantity", item)
             self.assertIn("item_total", item)
 
+    def test_access_permission_by_regular_user(self):
+        self.check_access_permission_by_regular_user()
+
+    def test_access_permission_by_anonymous_user(self):
+        self.check_access_permission_by_anonymous_user()
+
+    def test_list(self):
+        response = self.send_request()
+        self.validate_response_body(response)
+
     def test_list_empty_items(self):
         cart_id = CartFactory.create_cart()
-
-        # make request
-        response = self.client.get(
+        response = self.send_request(
             reverse("cart-items-list", kwargs={"cart_pk": cart_id})
         )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.expected_status_code(response)
 
-    def test_list_items_with_invalid_cart_id(self):
-        response = self.client.get(reverse("cart-items-list", kwargs={"cart_pk": 11}))
+    def test_list_with_invalid_cart_id(self):
+        response = self.send_request(reverse("cart-items-list", kwargs={"cart_pk": 11}))
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
 
-class RetrieveCartItemTest(APITestCaseMixin):
+class RetrieveCartItemTest(APIGetTestCaseMixin):
     @classmethod
     def setUpTestData(cls):
         super().setUpTestData()
         cls.cart_id, cls.cart_item = CartFactory.add_one_item(get_item=True)
 
-    # -------------------------------
-    # --- Test Access Permissions ---
-    # -------------------------------
-
-    def test_retrieve_item_by_admin(self):
-        self.set_admin_user_authorization()
-        response = self.client.get(
-            reverse(
-                "cart-items-detail",
-                kwargs={"cart_pk": self.cart_id, "pk": self.cart_item.id},
-            )
+    def api_path(self) -> str:
+        return reverse(
+            "cart-items-detail",
+            kwargs={"cart_pk": self.cart_id, "pk": self.cart_item.id},
         )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_retrieve_item_by_regular_user(self):
-        self.set_regular_user_authorization()
-        response = self.client.get(
-            reverse(
-                "cart-items-detail",
-                kwargs={"cart_pk": self.cart_id, "pk": self.cart_item.id},
-            )
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_retrieve_item_by_anonymous_user(self):
-        self.set_anonymous_user_authorization()
-        response = self.client.get(
-            reverse(
-                "cart-items-detail",
-                kwargs={"cart_pk": self.cart_id, "pk": self.cart_item.id},
-            )
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    # --------------------------
-    # --- Test Retrieve Item ---
-    # --------------------------
-
-    def test_retrieve_item(self):
-        cart_id, cart_item = CartFactory.add_one_item(get_item=True)
-
-        # make request
-        response = self.client.get(
-            reverse(
-                "cart-items-detail",
-                kwargs={"cart_pk": cart_id, "pk": cart_item.id},
-            )
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-        # expected
-        expected_cart_item = response.json()
+    def validate_response_body(self, response, payload: dict = None):
+        super().validate_response_body(response, payload)
+        expected_cart_item = self.response
         self.assertIn("id", expected_cart_item)
         self.assertIn("variant", expected_cart_item)
         variant = expected_cart_item["variant"]
@@ -152,10 +83,24 @@ class RetrieveCartItemTest(APITestCaseMixin):
         self.assertIn("quantity", expected_cart_item)
         self.assertIn("item_total", expected_cart_item)
 
-    def test_retrieve_item_with_invalid_cart_id(self):
-        cart_id, cart_item = CartFactory.add_one_item(get_item=True)
+    def test_access_permission_by_regular_user(self):
+        self.check_access_permission_by_regular_user()
 
-        # make request
+    def test_access_permission_by_anonymous_user(self):
+        self.check_access_permission_by_anonymous_user()
+
+    def test_retrieve(self):
+        cart_id, cart_item = CartFactory.add_one_item(get_item=True)
+        response = self.send_request(
+            reverse(
+                "cart-items-detail",
+                kwargs={"cart_pk": cart_id, "pk": cart_item.id},
+            )
+        )
+        self.validate_response_body(response)
+
+    def test_retrieve_with_invalid_cart_id(self):
+        cart_id, cart_item = CartFactory.add_one_item(get_item=True)
         response = self.client.get(
             reverse("cart-items-detail", kwargs={"cart_pk": 11, "pk": cart_item.id})
         )
