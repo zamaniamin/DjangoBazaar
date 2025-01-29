85c104fd267aa7c9bbde80d6f77b0db31e636d04
AminZamani <aminzamani.work@gmail.com>
2025-01-29 15:52:00 +0330

    test(shop/attribute): refactor retrieve attribute items
    version: 0.6.48-alpha

--- apps/shop/tests/test_attribute/test_attribute_items/test_retrieve_attribute_items.py
+++ apps/shop/tests/test_attribute/test_attribute_items/test_retrieve_attribute_items.py
@@ -1,72 +1,29 @@
 from django.urls import reverse
 from rest_framework import status
 
-from apps.core.tests.mixin import APITestCaseMixin
+from apps.core.tests.mixin import APIGetTestCaseMixin
 from apps.shop.demo.factory.attribute.attribute_factory import AttributeFactory
 
 
-class ListAttributeItemsTestMixin(APITestCaseMixin):
+class ListAttributeItemsTest(APIGetTestCaseMixin):
     @classmethod
     def setUpTestData(cls):
         super().setUpTestData()
         cls.attribute = AttributeFactory.create_attribute()
         cls.attribute_items = AttributeFactory.add_attribute_item_list(cls.attribute.id)
 
-    def setUp(self):
-        self.set_admin_user_authorization()
-
-    # -------------------------------
-    # --- Test Access Permissions ---
-    # -------------------------------
-
-    def test_list_items_by_admin(self):
-        response = self.client.get(
-            reverse(
-                "attribute-items-list",
-                kwargs={"attribute_pk": self.attribute.id},
-            )
+    def api_path(self) -> str:
+        return reverse(
+            "attribute-items-list",
+            kwargs={"attribute_pk": self.attribute.id},
         )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_list_items_by_regular_user(self):
-        self.set_regular_user_authorization()
-        response = self.client.get(
-            reverse(
-                "attribute-items-list",
-                kwargs={"attribute_pk": self.attribute.id},
-            )
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_list_items_by_anonymous_user(self):
-        self.set_anonymous_user_authorization()
-        response = self.client.get(
-            reverse(
-                "attribute-items-list",
-                kwargs={"attribute_pk": self.attribute.id},
-            )
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    # -----------------------
-    # --- Test List Items ---
-    # -----------------------
-
-    def test_list_items(self):
-        # make request
-        response = self.client.get(
-            reverse(
-                "attribute-items-list",
-                kwargs={"attribute_pk": self.attribute.id},
-            )
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-        # expected
-        expected_attribute_items = response.json()
-        self.assertEqual(len(expected_attribute_items), len(self.attribute_items))
-
-        for item in expected_attribute_items:
+    def validate_response_body(
+        self, response, payload: dict = None, item_list_len: int = 0
+    ):
+        super().validate_response_body(response, payload)
+        self.assertEqual(len(self.response), item_list_len)
+        for item in self.response:
             self.assertEqual(
                 set(item.keys()),
                 {
@@ -75,107 +32,70 @@ class ListAttributeItemsTestMixin(APITestCaseMixin):
                 },
             )
 
-    def test_list_items_if_attribute_not_exist(self):
-        response = self.client.get(
+    def test_access_permission_by_regular_user(self):
+        self.check_access_permission_by_regular_user()
+
+    def test_access_permission_by_anonymous_user(self):
+        self.check_access_permission_by_anonymous_user()
+
+    def test_list(self):
+        response = self.send_request()
+        self.validate_response_body(response, item_list_len=2)
+
+    def test_list_if_attribute_not_exist(self):
+        response = self.send_request(
             reverse("attribute-items-list", kwargs={"attribute_pk": 999})
         )
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-    def test_list_items_if_attribute_dont_have_item(self):
+    def test_list_if_attribute_dont_have_item(self):
         # create an attribute without items
         attribute = AttributeFactory.create_attribute("material")
 
-        # request
-        response = self.client.get(
+        response = self.send_request(
             reverse("attribute-items-list", kwargs={"attribute_pk": attribute.id})
         )
-
-        # expected
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        expected = response.json()
-        self.assertEqual(len(expected), 0)
+        self.validate_response_body(response)
 
 
-class RetrieveAttributeItemTestMixin(APITestCaseMixin):
+class RetrieveAttributeItemsTest(APIGetTestCaseMixin):
     @classmethod
     def setUpTestData(cls):
         super().setUpTestData()
         cls.attribute = AttributeFactory.create_attribute()
         cls.attribute_item = AttributeFactory.add_one_attribute_item(cls.attribute.id)
 
-    def setUp(self):
-        self.set_admin_user_authorization()
-
-    # -------------------------------
-    # --- Test Access Permissions ---
-    # -------------------------------
-
-    def test_retrieve_item_by_admin(self):
-        response = self.client.get(
-            reverse(
-                "attribute-items-detail",
-                kwargs={
-                    "attribute_pk": self.attribute.id,
-                    "pk": self.attribute_item.id,
-                },
-            )
+    def api_path(self) -> str:
+        return reverse(
+            "attribute-items-detail",
+            kwargs={
+                "attribute_pk": self.attribute.id,
+                "pk": self.attribute_item.id,
+            },
         )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_retrieve_item_by_regular_user(self):
-        self.set_regular_user_authorization()
-        response = self.client.get(
-            reverse(
-                "attribute-items-detail",
-                kwargs={
-                    "attribute_pk": self.attribute.id,
-                    "pk": self.attribute_item.id,
-                },
-            )
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_retrieve_item_by_anonymous_user(self):
-        self.set_anonymous_user_authorization()
-        response = self.client.get(
-            reverse(
-                "attribute-items-detail",
-                kwargs={
-                    "attribute_pk": self.attribute.id,
-                    "pk": self.attribute_item.id,
-                },
-            )
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    # -----------------------------
-    # --- Test Retrieve an Item ---
-    # -----------------------------
-
-    def test_retrieve_item(self):
-        response = self.client.get(
-            reverse(
-                "attribute-items-detail",
-                kwargs={
-                    "attribute_pk": self.attribute.id,
-                    "pk": self.attribute_item.id,
-                },
-            )
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-        # expected
-        expected = response.json()
+    def validate_response_body(self, response, payload: dict = None):
+        super().validate_response_body(response, payload)
         self.assertEqual(
-            set(expected.keys()),
+            set(self.response.keys()),
             {
                 "id",
                 "item_name",
             },
         )
 
-    def test_retrieve_item_if_attribute_not_exist(self):
-        response = self.client.get(
+    def test_access_permission_by_regular_user(self):
+        self.check_access_permission_by_regular_user()
+
+    def test_access_permission_by_anonymous_user(self):
+        self.check_access_permission_by_anonymous_user()
+
+    def test_retrieve(self):
+        response = self.send_request()
+        self.validate_response_body(response)
+
+    def test_retrieve_if_attribute_not_exist(self):
+        response = self.send_request(
             reverse(
                 "attribute-items-detail",
                 kwargs={"attribute_pk": 999, "pk": self.attribute_item.id},
@@ -183,8 +103,8 @@ class RetrieveAttributeItemTestMixin(APITestCaseMixin):
         )
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-    def test_retrieve_item_if_item_not_exist(self):
-        response = self.client.get(
+    def test_retrieve_if_item_not_exist(self):
+        response = self.send_request(
             reverse(
                 "attribute-items-detail",
                 kwargs={"attribute_pk": self.attribute.id, "pk": 999},
