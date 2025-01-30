bcf061f6f338d0a1dafae8c96e280024b7575ea8
AminZamani <aminzamani.work@gmail.com>
2025-01-30 05:43:40 +0330

    test(shop/options): refactor update option
    version: 0.6.48-alpha

--- apps/shop/tests/test_option/test_update_option.py
+++ apps/shop/tests/test_option/test_update_option.py
@@ -1,82 +1,24 @@
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.utils import json
 
-from apps.core.tests.mixin import APITestCaseMixin
+from apps.core.tests.mixin import APIUpdateTestCaseMixin
 from apps.shop.demo.factory.option.option_factory import OptionFactory
 
 
-class UpdateOptionTest(APITestCaseMixin):
+class UpdateOptionTest(APIUpdateTestCaseMixin):
     @classmethod
     def setUpTestData(cls):
         super().setUpTestData()
         cls.option = OptionFactory.create_option()
 
-    def setUp(self):
-        self.set_admin_user_authorization()
-
-    # -------------------------------
-    # --- Test Access Permissions ---
-    # -------------------------------
-
-    def test_update_option_by_admin(self):
-        # request
-        payload = {"option_name": "black"}
-        response = self.client.put(
-            reverse(
-                "option-detail",
-                kwargs={"pk": self.option.id},
-            ),
-            json.dumps(payload),
-            content_type="application/json",
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_update_option_by_regular_user(self):
-        self.set_regular_user_authorization()
-        response = self.client.put(
-            reverse(
-                "option-detail",
-                kwargs={"pk": self.option.id},
-            ),
-            content_type="application/json",
-        )
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-    def test_update_option_by_anonymous_user(self):
-        self.set_anonymous_user_authorization()
-        response = self.client.put(
-            reverse(
-                "option-detail",
-                kwargs={"pk": self.option.id},
-            ),
-            content_type="application/json",
-        )
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-
-    # ---------------------
-    # --- Update Option ---
-    # ---------------------
-
-    def test_update_option(self):
-        # get old option name
-        old_option_name = self.option.option_name
-
-        # request
-        new_option_name = "color2"
-        payload = {"option_name": new_option_name}
-        response = self.client.put(
-            reverse(
-                "option-detail",
-                kwargs={"pk": self.option.id},
-            ),
-            json.dumps(payload),
-            content_type="application/json",
+    def api_path(self) -> str:
+        return reverse(
+            "option-detail",
+            kwargs={"pk": self.option.id},
         )
 
-        # expected
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        expected = response.json()
+    def validate_response_body(self, response, payload):
+        super().validate_response_body(response, payload)
         self.assertEqual(
             set(response.data.keys()),
             {
@@ -86,18 +28,24 @@ class UpdateOptionTest(APITestCaseMixin):
                 "created_at",
             },
         )
-        self.assertEqual(expected["option_name"], new_option_name)
-        self.assertNotEqual(old_option_name, new_option_name)
+        self.assertEqual(self.response["option_name"], payload.get("option_name"))
 
-    def test_update_if_option_not_exist(self):
-        # request
-        response = self.client.put(
-            reverse(
+    def test_access_permission_by_regular_user(self):
+        self.check_access_permission_by_regular_user()
+
+    def test_access_permission_by_anonymous_user(self):
+        self.check_access_permission_by_anonymous_user()
+
+    def test_update(self):
+        payload = {"option_name": "color2"}
+        response = self.send_request(payload)
+        self.validate_response_body(response, payload)
+
+    def test_update_404(self):
+        response = self.send_request(
+            path=reverse(
                 "option-detail",
                 kwargs={"pk": 999},
-            ),
-            content_type="application/json",
+            )
         )
-
-        # expected
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
