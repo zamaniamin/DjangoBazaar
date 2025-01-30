f95d60187abd49128a81240c2dc29d3783ada5cd
AminZamani <aminzamani.work@gmail.com>
2025-01-30 04:04:45 +0330

    test(shop/cart): refactor create cart test case
    version: 0.6.48-alpha

--- apps/shop/tests/test_cart/test_create_cart.py
+++ apps/shop/tests/test_cart/test_create_cart.py
@@ -3,44 +3,27 @@ import uuid
 from django.urls import reverse
 from rest_framework import status
 
-from apps.core.tests.mixin import APITestCaseMixin
+from apps.core.tests.mixin import APIPostTestCaseMixin
 
 
-class CreateCartTest(APITestCaseMixin):
-    # ------------------------------
-    # --- Test Access Permission ---
-    # ------------------------------
+class CreateCartTest(APIPostTestCaseMixin):
+    def api_path(self) -> str:
+        return reverse("cart-list")
 
-    def test_create_cart_by_admin(self):
-        self.set_admin_user_authorization()
-        response = self.client.post(reverse("cart-list"))
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    def validate_response_body(self, response, payload):
+        super().validate_response_body(response, payload)
+        self.assertEqual(len(self.response), 3)
+        self.assertIsInstance(uuid.UUID(self.response["id"]), uuid.UUID)
+        self.assertIsInstance(self.response["items"], list)
+        self.assertEqual(len(self.response["items"]), 0)
+        self.assertEqual(self.response["total_price"], 0)
 
-    def test_create_cart_by_regular_user(self):
-        self.set_regular_user_authorization()
-        response = self.client.post(reverse("cart-list"))
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    def test_access_permission_by_regular_user(self):
+        self.check_access_permission_by_regular_user(status.HTTP_201_CREATED)
 
-    def test_create_cart_by_anonymous_user(self):
-        self.set_anonymous_user_authorization()
-        response = self.client.post(reverse("cart-list"))
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
-    # --------------------------
-    # --- Test Create a Cart ---
-    # --------------------------
+    def test_access_permission_by_anonymous_user(self):
+        self.check_access_permission_by_anonymous_user(status.HTTP_201_CREATED)
 
     def test_create_cart(self):
-        # request
-        response = self.client.post(
-            reverse("cart-list"), content_type="application/json"
-        )
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
-        # expected
-        expected = response.json()
-        self.assertEqual(len(expected), 3)
-        self.assertIsInstance(uuid.UUID(expected["id"]), uuid.UUID)
-        self.assertIsInstance(expected["items"], list)
-        self.assertEqual(len(expected["items"]), 0)
-        self.assertEqual(expected["total_price"], 0)
+        response = self.send_request()
+        self.validate_response_body(response, {})
