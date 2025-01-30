f116e1cac484a3536c5a418859710552ada1d7c6
AminZamani <aminzamani.work@gmail.com>
2025-01-30 06:34:53 +0330

    test(core/test): remove extra class
    version: 0.6.48-alpha

--- apps/core/tests/mixin.py
+++ apps/core/tests/mixin.py
@@ -1,6 +1,4 @@
 import json
-import os
-import re
 from abc import ABC, abstractmethod
 from datetime import datetime
 
@@ -10,70 +8,6 @@ from rest_framework.test import APITestCase
 from apps.core.demo.factory.user_factory import UserFactory
 from apps.core.services.time_service import DateTime
 from apps.core.services.token_service import TokenService
-from config import settings
-
-
-class _APITestCaseMixin_(APITestCase):
-    @classmethod
-    def setUpTestData(cls):
-        # Runs once per test class.
-
-        # create users
-        cls.admin = UserFactory.create(is_staff=True)
-        cls.admin_access_token = TokenService.jwt_get_access_token(cls.admin)
-
-        cls.regular_user = UserFactory.create()
-        cls.regular_user_access_token = TokenService.jwt_get_access_token(
-            cls.regular_user
-        )
-
-    def set_admin_user_authorization(self):
-        self.client.credentials(HTTP_AUTHORIZATION=f"JWT {self.admin_access_token}")
-
-    def set_regular_user_authorization(self):
-        self.client.credentials(
-            HTTP_AUTHORIZATION=f"JWT {self.regular_user_access_token}"
-        )
-
-    def set_anonymous_user_authorization(self):
-        self.client.credentials()
-
-    def assertImageSrcPattern(self, src):
-        # Define the regex pattern for the src
-        pattern = r"^http://.*media.*$"
-
-        # Assert that the src matches the pattern
-        self.assertTrue(
-            re.match(pattern, src), f"src '{src}' does not match the expected pattern"
-        )
-
-    def assertImageFileDirectory(self, src):
-        """Assert that the image file exists in the correct directory based on the src URL."""
-
-        # Extract the relative file path from the src
-        match = re.search(r"testserver/(.*)", src)
-        self.assertIsNotNone(match, "The src does not contain a valid path.")
-
-        # Build the full path based on the extracted path and the base directory
-        relative_path = match.group(1)
-        full_file_path = os.path.join(settings.BASE_DIR, relative_path)
-
-        # Assert that the file exists at the generated full path
-        self.assertTrue(
-            os.path.exists(full_file_path), f"File does not exist: {full_file_path}"
-        )
-
-    def post_json(self, url, data: dict = None, **kwargs):
-        if data is None:
-            data = {}
-        return self.client.post(
-            url, data=json.dumps(data), content_type="application/json", **kwargs
-        )
-
-    def post_multipart(self, url, data: dict = None, **kwargs):
-        if data is None:
-            data = {}
-        return self.client.post(url, data, format="multipart", **kwargs)
 
 
 class _APITestCaseAuthorizationMixin(APITestCase):
