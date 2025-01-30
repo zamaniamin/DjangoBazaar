74685a84ad85f851feba0d9aa1e97764682fa8cf
AminZamani <aminzamani.work@gmail.com>
2025-01-30 06:10:34 +0330

    test(core/user): refactor update user
    version: 0.6.48-alpha

--- apps/core/tests/test_user/test_partial_update_profile.py
+++ apps/core/tests/test_user/test_partial_update_profile.py
@@ -1,71 +1,51 @@
 import json
 
 from django.urls import reverse
-from rest_framework import status
 
-from apps.core.tests.mixin import APITestCaseMixin
+from apps.core.tests.mixin import APIUpdateTestCaseMixin
 
 
-class PartialUpdateProfileTestMixin(APITestCaseMixin):
-    # ------------------------------
-    # --- Test Access Permission ---
-    # ------------------------------
+class PartialUpdateProfileTest(APIUpdateTestCaseMixin):
+    def api_path(self) -> str:
+        return reverse("user-me")
 
-    def test_partial_update_profile_by_admin(self):
-        # request
-        self.set_admin_user_authorization()
+    def validate_response_body(self, response, payload):
+        super().validate_response_body(response, payload)
+        self.assertEqual(
+            set(self.response.keys()),
+            {
+                "id",
+                "email",
+                "first_name",
+                "last_name",
+                "is_active",
+                "is_staff",
+                "date_joined",
+                "last_login",
+            },
+        )
+
+    def test_access_permission_by_regular_user(self):
+        self.authorization_as_regular_user()
+        payload = {"first_name": "member f name"}
+        response = self.client.patch(
+            self.api_path(),
+            json.dumps(payload),
+            content_type="application/json",
+        )
+        self.validate_response_body(response, payload)
+
+    def test_access_permission_by_anonymous_user(self):
+        self.check_access_permission_by_anonymous_user()
+
+    def test_partial_update(self):
         payload = {
             "first_name": "admin f name",
             "last_name": "admin l name",
         }
         response = self.client.patch(
-            reverse("user-me"),
+            self.api_path(),
             json.dumps(payload),
             content_type="application/json",
         )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-        # expected
-        self.assertEqual(
-            set(response.data.keys()),
-            {
-                "id",
-                "email",
-                "first_name",
-                "last_name",
-                "is_active",
-                "is_staff",
-                "date_joined",
-                "last_login",
-            },
-        )
-
-    def test_partial_update_profile_by_regular_user(self):
-        # request
-        self.set_regular_user_authorization()
-        payload = {"first_name": "member f name"}
-        response = self.client.patch(
-            reverse("user-me"),
-            json.dumps(payload),
-            content_type="application/json",
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-        # expected
-        self.assertEqual(
-            set(response.data.keys()),
-            {
-                "id",
-                "email",
-                "first_name",
-                "last_name",
-                "is_active",
-                "is_staff",
-                "date_joined",
-                "last_login",
-            },
-        )
-
-    def test_partial_update_profile_by_anonymous_user(self):
-        response = self.client.patch(reverse("user-me"))
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.validate_response_body(response, payload)
