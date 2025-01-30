2687deb636af3b43b0788f8288ee84da32876c8a
AminZamani <aminzamani.work@gmail.com>
2025-01-30 05:01:37 +0330

    test(shop/options): refactor delete option item
    version: 0.6.48-alpha

--- apps/shop/tests/test_option/test_option_items/test_destroy_option_item.py
+++ apps/shop/tests/test_option/test_option_items/test_destroy_option_item.py
@@ -1,78 +1,39 @@
 from django.urls import reverse
 from rest_framework import status
 
-from apps.core.tests.mixin import APITestCaseMixin
+from apps.core.tests.mixin import APIDeleteTestCaseMixin
 from apps.shop.demo.factory.option.option_factory import OptionFactory
 
 
-class DestroyOptionItemsTest(APITestCaseMixin):
+class DestroyOptionItemsTest(APIDeleteTestCaseMixin):
     @classmethod
     def setUpTestData(cls):
         super().setUpTestData()
         cls.option = OptionFactory.create_option()
         cls.option_item = OptionFactory.add_one_option_item(cls.option.id)
 
-    def setUp(self):
-        self.set_admin_user_authorization()
-
-    # -------------------------------
-    # --- Test Access Permissions ---
-    # -------------------------------
-
-    def test_delete_item_by_admin(self):
-        response = self.client.delete(
-            reverse(
-                "option-items-detail",
-                kwargs={"option_pk": self.option.id, "pk": self.option_item.id},
-            )
+    def api_path(self) -> str:
+        return reverse(
+            "option-items-detail",
+            kwargs={"option_pk": self.option.id, "pk": self.option_item.id},
         )
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
-    def test_delete_item_by_regular_user(self):
-        self.set_regular_user_authorization()
-        response = self.client.delete(
-            reverse(
-                "option-items-detail",
-                kwargs={"option_pk": self.option.id, "pk": self.option_item.id},
-            )
-        )
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    def test_access_permission_by_regular_user(self):
+        self.check_access_permission_by_regular_user()
 
-    def test_delete_item_by_anonymous_user(self):
-        self.set_anonymous_user_authorization()
-        response = self.client.delete(
-            reverse(
-                "option-items-detail",
-                kwargs={"option_pk": self.option.id, "pk": self.option_item.id},
-            )
-        )
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+    def test_access_permission_by_anonymous_user(self):
+        self.check_access_permission_by_anonymous_user()
 
-    # ---------------------------
-    # --- Test Delete an item ---
-    # ---------------------------
-
-    def test_delete_item(self):
-        # delete an option item
-        response = self.client.delete(
-            reverse(
-                "option-items-detail",
-                kwargs={"option_pk": self.option.id, "pk": self.option_item.id},
-            )
-        )
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+    def test_delete(self):
+        response = self.send_request()
+        self.expected_status_code(response)
 
         # check that option item was deleted
-        response = self.client.get(
-            reverse(
-                "option-items-detail",
-                kwargs={"option_pk": self.option.id, "pk": self.option_item.id},
-            )
-        )
+        response = self.client.get(self.api_path())
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-    def test_delete_item_with_invalid_option_pk(self):
-        response = self.client.delete(
+    def test_delete_with_invalid_option_pk(self):
+        response = self.send_request(
             reverse(
                 "option-items-detail",
                 kwargs={"option_pk": 999, "pk": self.option_item.id},
@@ -80,8 +41,8 @@ class DestroyOptionItemsTest(APITestCaseMixin):
         )
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-    def test_delete_item_with_invalid_item_pk(self):
-        response = self.client.delete(
+    def test_delete_with_invalid_item_pk(self):
+        response = self.send_request(
             reverse(
                 "option-items-detail",
                 kwargs={"option_pk": self.option.id, "pk": 999},
