0a46fc7231d02269c8b48505da722d5df21eb42e
AminZamani <aminzamani.work@gmail.com>
2025-01-30 06:22:34 +0330

    test(core/user): refactor retrieve user
    version: 0.6.48-alpha

--- apps/core/tests/test_user/test_retrieve_user.py
+++ apps/core/tests/test_user/test_retrieve_user.py
@@ -1,46 +1,33 @@
 from django.urls import reverse
 from rest_framework import status
 
-from apps.core.tests.mixin import APITestCaseMixin
+from apps.core.tests.mixin import APIGetTestCaseMixin, APIAssertMixin
 
 
-class RetrieveUserTestMixin(APITestCaseMixin):
-    # ------------------------------
-    # --- Test Access Permission ---
-    # ------------------------------
+class RetrieveUserTest(APIGetTestCaseMixin, APIAssertMixin):
+    def api_path(self) -> str:
+        return reverse("user-detail", kwargs={"pk": self.regular_user.id})
 
-    def test_retrieve_user_by_admin(self):
-        self.set_admin_user_authorization()
-
-        # request
-        response = self.client.get(
-            reverse("user-detail", kwargs={"pk": self.regular_user.id})
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-        # expected
-        expected = response.json()
-        self.assertEqual(len(expected), 7)
-        self.assertIsInstance(expected["id"], int)
-        self.assertIsInstance(expected["email"], str)
-        self.assertIsInstance(expected["first_name"], str)
-        self.assertIsInstance(expected["last_name"], str)
-        self.assertIsInstance(expected["is_active"], bool)
-        self.assertDatetimeFormat(expected["date_joined"])
+    def validate_response_body(self, response, payload: dict = None):
+        super().validate_response_body(response, payload)
+        self.assertEqual(len(self.response), 7)
+        self.assertIsInstance(self.response["id"], int)
+        self.assertIsInstance(self.response["email"], str)
+        self.assertIsInstance(self.response["first_name"], str)
+        self.assertIsInstance(self.response["last_name"], str)
+        self.assertIsInstance(self.response["is_active"], bool)
+        self.assertDatetimeFormat(self.response["date_joined"])
         self.assertTrue(
-            expected["last_login"] is None
-            or self.assertDatetimeFormat(expected["last_login"])
+            self.response["last_login"] is None
+            or self.assertDatetimeFormat(self.response["last_login"])
         )
 
-    def test_retrieve_user_by_regular_user(self):
-        self.set_regular_user_authorization()
-        response = self.client.get(
-            reverse("user-detail", kwargs={"pk": self.regular_user.id})
-        )
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    def test_access_permission_by_regular_user(self):
+        self.check_access_permission_by_regular_user(status.HTTP_403_FORBIDDEN)
 
-    def test_retrieve_user_by_anonymous_user(self):
-        response = self.client.get(
-            reverse("user-detail", kwargs={"pk": self.regular_user.id})
-        )
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+    def test_access_permission_by_anonymous_user(self):
+        self.check_access_permission_by_anonymous_user(status.HTTP_401_UNAUTHORIZED)
+
+    def test_retrieve(self):
+        response = self.send_request()
+        self.validate_response_body(response)
