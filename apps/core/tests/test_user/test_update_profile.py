dcb7daa7eb44517f3bc393838fbd03034c83d133
AminZamani <aminzamani.work@gmail.com>
2025-01-30 06:26:26 +0330

    test(core/user): refactor update user profile
    version: 0.6.48-alpha

--- apps/core/tests/test_user/test_update_profile.py
+++ apps/core/tests/test_user/test_update_profile.py
@@ -1,71 +1,39 @@
-import json
-
 from django.urls import reverse
 from rest_framework import status
 
-from apps.core.tests.mixin import APITestCaseMixin
+from apps.core.tests.mixin import APIUpdateTestCaseMixin
 
 
-class UpdateProfileTestMixin(APITestCaseMixin):
-    # ------------------------------
-    # --- Test Access Permission ---
-    # ------------------------------
+class UpdateProfileTest(APIUpdateTestCaseMixin):
+    def api_path(self) -> str:
+        return reverse("user-me")
 
-    def test_update_profile_by_admin(self):
-        # request
-        self.set_admin_user_authorization()
+    def validate_response_body(self, response, payload):
+        super().validate_response_body(response, payload)
+        self.assertEqual(
+            set(self.response.keys()),
+            {
+                "id",
+                "email",
+                "first_name",
+                "last_name",
+                "is_active",
+                "is_staff",
+                "date_joined",
+                "last_login",
+            },
+        )
+
+    def test_access_permission_by_regular_user(self):
+        self.check_access_permission_by_regular_user(status.HTTP_200_OK)
+
+    def test_access_permission_by_anonymous_user(self):
+        self.check_access_permission_by_anonymous_user(status.HTTP_401_UNAUTHORIZED)
+
+    def test_update_profile(self):
         payload = {
             "first_name": "admin f name",
             "last_name": "admin l name",
         }
-        response = self.client.put(
-            reverse("user-me"),
-            json.dumps(payload),
-            content_type="application/json",
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-        # expected
-        self.assertEqual(
-            set(response.data.keys()),
-            {
-                "id",
-                "email",
-                "first_name",
-                "last_name",
-                "is_active",
-                "is_staff",
-                "date_joined",
-                "last_login",
-            },
-        )
-
-    def test_update_profile_by_regular_user(self):
-        # request
-        self.set_regular_user_authorization()
-        payload = {"first_name": "member f name"}
-        response = self.client.put(
-            reverse("user-me"),
-            json.dumps(payload),
-            content_type="application/json",
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-        # expected
-        self.assertEqual(
-            set(response.data.keys()),
-            {
-                "id",
-                "email",
-                "first_name",
-                "last_name",
-                "is_active",
-                "is_staff",
-                "date_joined",
-                "last_login",
-            },
-        )
-
-    def test_update_profile_by_anonymous_user(self):
-        response = self.client.put(reverse("user-me"))
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        response = self.send_request()
+        self.validate_response_body(response, payload)
