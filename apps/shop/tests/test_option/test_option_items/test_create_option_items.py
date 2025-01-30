75ca17b4eb6de8e4d3efc544c3249ec3c56494af
AminZamani <aminzamani.work@gmail.com>
2025-01-30 20:50:25 +0330

    test(shop/option): refactor create option
    version: 0.6.48-alpha

--- apps/shop/tests/test_option/test_option_items/test_create_option_items.py
+++ apps/shop/tests/test_option/test_option_items/test_create_option_items.py
@@ -32,15 +32,15 @@ class CreateOptionItemsTest(APIPostTestCaseMixin):
     def test_create_if_already_exist(self):
         OptionFactory.add_one_option_item(self.option.id)
         response = self.send_request(self.payload)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertHTTPStatusCode(response, status.HTTP_400_BAD_REQUEST)
 
     def test_create_if_item_name_is_empty(self):
         payload = {"item_name": ""}
         response = self.send_request(payload)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertHTTPStatusCode(response, status.HTTP_400_BAD_REQUEST)
 
     def test_create_if_option_not_exist(self):
         response = self.send_request(
             self.payload, reverse("option-items-list", kwargs={"option_pk": 999})
         )
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertHTTPStatusCode(response, status.HTTP_404_NOT_FOUND)
