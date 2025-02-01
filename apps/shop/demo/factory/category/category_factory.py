8dbf625d5d131ef80eefee1ffa9c08210b2bace0
AminZamani <aminzamani.work@gmail.com>
2025-02-01 05:59:12 +0330

    test(shop/category): add new method to generate child category
    version: 0.6.50-alpha

--- apps/shop/demo/factory/category/category_factory.py
+++ apps/shop/demo/factory/category/category_factory.py
@@ -1,4 +1,4 @@
-from factory import LazyAttribute
+from factory import LazyAttribute, post_generation
 from factory.django import DjangoModelFactory
 from faker import Faker
 
@@ -15,3 +15,18 @@ class CategoryFactory(DjangoModelFactory):
     description = LazyAttribute(lambda _: fake.sentence())
     slug = LazyAttribute(lambda obj: fake.slug())
     parent = None  # Set to None by default, you can override in tests
+
+    @post_generation
+    def children(self, create, extracted, **kwargs):
+        """
+        Optional children relationship.
+        You can pass a number like CategoryFactory(children=3) to create 3 children.
+        """
+        if not create:
+            # Simple build, do nothing.
+            return
+
+        if extracted:
+            # A list of children were passed in, so create them
+            for child in range(extracted):
+                CategoryFactory(parent=self)
