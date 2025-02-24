1bc1b231039c5d4f4d39723b1199c8c4409d8bbe
AminZamani <aminzamani.work@gmail.com>
2025-02-25 01:14:28 +0330

    refactor(shop): Rename `product_url.py` to `products_urls.py`
    - Rename `product_url.py` to `products_urls.py` for consistency.
    - Update import reference in `shop_url.py` to reflect the new filename.
    
    Version: 0.6.55-alpha

--- apps/shop/urls/shop_url.py
+++ apps/shop/urls/shop_url.py
@@ -4,7 +4,7 @@ urlpatterns = [
     path("options/", include("apps.shop.urls.options_urls")),
     path("categories/", include("apps.shop.urls.categories_urls")),
     path("attributes/", include("apps.shop.urls.attributes_urls")),
-    path("products/", include("apps.shop.urls.product_url")),
+    path("products/", include("apps.shop.urls.products_urls")),
     path("variants/", include("apps.shop.urls.variants_urls")),
     path("carts/", include("apps.shop.urls.carts_urls")),
 ]
