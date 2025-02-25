99edd7b40a53e70dd15c053ac5a4e6d6af520fd7
AminZamani <aminzamani.work@gmail.com>
2025-02-25 20:37:58 +0330

    feat(core/user): Add CRUD operations in UserService
    - Implement methods for create, retrieve, update, and delete user.
    - Add a method to list all users.
    
    Version: 0.6.55-alpha

--- apps/core/services/user/user_service.py
+++ apps/core/services/user/user_service.py
@@ -1,11 +1,24 @@
 from apps.core.models import User
 
 
+# CRUD
 class UserService:
     @classmethod
     def create_user(cls, **data):
         pass
 
     @classmethod
+    def retrieve_user(cls, user: User, **data):
+        pass
+
+    @classmethod
+    def list_users(cls, **data):
+        pass
+
+    @classmethod
     def update_user(cls, user: User, **data):
+        pass
+
+    @classmethod
+    def delete_user(cls, user: User, **data):
         pass
